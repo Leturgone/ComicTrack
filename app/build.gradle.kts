0ba7e0b1ed4265f1cf6d4c504e0db2c7283be67b
Leturgone <finekon221@gmail.com>
2025-02-08 16:45:44 +0300

    Добавлены зависимомти дял Coil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.7.5")
     implementation("androidx.navigation:navigation-compose:2.8.3")
     implementation("com.maxkeppeler.sheets-compose-dialogs:calendar:1.0.2")
+
+    implementation("io.coil-kt.coil3:coil-compose:3.0.4")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
 }
