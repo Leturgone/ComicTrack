a4e0b37c9e7f85d367d7321cfca22e2eb2b54740
Leturgone <finekon221@gmail.com>
2025-02-09 17:15:12 +0300

    Добавлены заготовки для функций

--- app/src/main/java/com/example/comictracker/searchScreen/MayLikeSec.kt
+++ app/src/main/java/com/example/comictracker/searchScreen/MayLikeSec.kt
@@ -1,2 +1,8 @@
 package com.example.comictracker.searchScreen
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MayLikeSec(){
+
+}
--- app/src/main/java/com/example/comictracker/searchScreen/СharacterSec.kt
+++ app/src/main/java/com/example/comictracker/searchScreen/СharacterSec.kt
@@ -1,2 +1,9 @@
 package com.example.comictracker.searchScreen
 
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun CharacterSec(){
+
+}
