9b808fa4cf456ed2beac8a02aa2c4025290e6e9e
Leturgone <finekon221@gmail.com>
2025-02-11 10:51:23 +0300

    Сделан LibaryScreen с прокурткой

--- app/src/main/java/com/example/comictracker/libaryScreen/LibraryScreen.kt
+++ app/src/main/java/com/example/comictracker/libaryScreen/LibraryScreen.kt
@@ -1,13 +1,32 @@
 package com.example.comictracker.libaryScreen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@Preview
 @Composable
 fun LibraryScreen(){
     Column {
-        FavoriteSec()
-        CurrentReadingSec()
-        LatestReadingSec()
+        Text(text = "My Library",
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp))
+        Column(Modifier.verticalScroll(rememberScrollState())) {
+            FavoriteSec()
+            CurrentReadingSec()
+            LatestReadingSec()
+        }
     }
+
 }
