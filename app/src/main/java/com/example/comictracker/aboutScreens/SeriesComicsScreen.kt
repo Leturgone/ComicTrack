62789ee32c4c8d0aefff30a1c7205e0d04d83c30
Leturgone <finekon221@gmail.com>
2025-02-14 22:25:35 +0300

    Сделана заготовка для функция SiriesScreen

---
+++ app/src/main/java/com/example/comictracker/aboutScreens/SeriesComicsScreen.kt
@@ -0,0 +1,12 @@
+package com.example.comictracker.aboutScreens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SeriesScreen(){
+    AboutSeriesSec()
+    UsersMarkSec()
+    NextComicSec()
+    RelatedSec()
+
+}
