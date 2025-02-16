5d66555049b973206f08c967b1919ae6f5764430
Leturgone <finekon221@gmail.com>
2025-02-16 12:41:57 +0300

    Сделана карточка элемнта списка комиксов серии

--- app/src/main/java/com/example/comictracker/aboutScreens/NextComicSec.kt
+++ app/src/main/java/com/example/comictracker/aboutScreens/NextComicSec.kt
@@ -1,9 +1,39 @@
 package com.example.comictracker.aboutScreens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.NavigateNext
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
 fun NextComicSec(){
+    Column {
+        Text(text = "Continue reading",
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp))
+
+        Box(modifier = Modifier.fillMaxWidth(),contentAlignment = Alignment.TopEnd){
+            Text(text = "See all",
+                color = MaterialTheme.colorScheme.primary, modifier = Modifier.padding(end = 15.dp, bottom = 12.dp))
+        }
+    }
 
 }
