8663abbe0bcba2645c0fe65649cffc2e76a52869
Leturgone <finekon221@gmail.com>
2025-02-16 17:08:17 +0300

    параметры с именами объединены в один

--- app/src/main/java/com/example/comictracker/data/CreatorsMember.kt
+++ app/src/main/java/com/example/comictracker/data/CreatorsMember.kt
@@ -1,8 +1,7 @@
 package com.example.comictracker.data
 
 data class CreatorsMember(
-    val firstName:String,
-    val lastName:String,
+    val name:String,
     val category: String,
     val image:String
 )
