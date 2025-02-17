1dba61f1dcde759ab84d28a77db515b88518cea7
Leturgone <finekon221@gmail.com>
2025-02-17 12:12:11 +0300

    Сделана секция со списком комиксов в серии

---
+++ app/src/main/java/com/example/comictracker/aboutScreens/aboutSeries/AllComicSeriesSec.kt
@@ -0,0 +1,41 @@
+package com.example.comictracker.aboutScreens.aboutSeries
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.comictracker.data.ComicCover
+@Preview
+@Composable
+
+fun AllComicSeriesSec(){
+    val comics = listOf(
+        ComicCover("Spider-Man Noir #1","http://i.annihil.us/u/prod/marvel/i/mg/5/e0/5bc77a942112a.jpg",null),
+        ComicCover("Spider-Man Noir #2","http://i.annihil.us/u/prod/marvel/i/mg/5/e0/5bc77a942112a.jpg",null),
+        ComicCover("Spider-Man Noir #3","http://i.annihil.us/u/prod/marvel/i/mg/5/e0/5bc77a942112a.jpg",null),
+        ComicCover("Spider-Man Noir #4","http://i.annihil.us/u/prod/marvel/i/mg/5/e0/5bc77a942112a.jpg",null),
+        ComicCover("Spider-Man Noir #5","http://i.annihil.us/u/prod/marvel/i/mg/5/e0/5bc77a942112a.jpg",null),
+        ComicCover("Spider-Man Noir #6","http://i.annihil.us/u/prod/marvel/i/mg/5/e0/5bc77a942112a.jpg",null),
+        ComicCover("Spider-Man Noir #7","http://i.annihil.us/u/prod/marvel/i/mg/5/e0/5bc77a942112a.jpg",null)
+    )
+    Column {
+        AboutSeriesSec()
+        Text(text = "All Comics ",
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp))
+        LazyColumn{
+            items(comics.size){
+                ComicFromSeriesCard()
+            }
+        }
+    }
+}
