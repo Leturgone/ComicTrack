b6d004a1ec6b178c7609b45cec4817c03cfdd6e8
Leturgone <finekon221@gmail.com>
2025-02-18 00:31:46 +0300

    Создан макет экрана о персонаже

--- app/src/main/java/com/example/comictracker/aboutScreens/aboutCharacter/AboutCharacterSec.kt
+++ app/src/main/java/com/example/comictracker/aboutScreens/aboutCharacter/AboutCharacterSec.kt
@@ -1,2 +1,17 @@
 package com.example.comictracker.aboutScreens.aboutCharacter
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.comictracker.AllComicScreen
+
+
+@Composable
+fun AboutCharacterScreen(){
+    Column(Modifier.verticalScroll(rememberScrollState())) {
+        CharacterSec()
+        AllComicScreen()
+    }
+}
