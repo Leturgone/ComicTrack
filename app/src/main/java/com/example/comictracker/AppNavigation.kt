a96ca9a6e0259fd101730b31506053eb3497f831
Leturgone <finekon221@gmail.com>
2025-02-19 19:45:26 +0300

    Добавлены пункты навигации для searchScreen

--- app/src/main/java/com/example/comictracker/AppNavigation.kt
+++ app/src/main/java/com/example/comictracker/AppNavigation.kt
@@ -13,6 +13,7 @@ import com.example.comictracker.aboutScreens.aboutSeries.AllComicSeriesSec
 import com.example.comictracker.aboutScreens.aboutSeries.SeriesScreen
 import com.example.comictracker.homeScreen.HomeScreen
 import com.example.comictracker.libaryScreen.LibraryScreen
+import com.example.comictracker.searchScreen.AllCharactersScreen
 import com.example.comictracker.searchScreen.SearchResultScreen
 import com.example.comictracker.searchScreen.SearchScreen
 
@@ -28,12 +29,13 @@ fun AppNavigation(innerPadding: PaddingValues, navController: NavHostController)
         composable("home"){ HomeScreen(navController)}
         composable("search"){ SearchScreen(navController)}
         composable("library"){ LibraryScreen(navController)}
-        composable("search_result") { SearchResultScreen() }
+        composable("search_result") { SearchResultScreen(navController) }
+        composable("all_characters"){ AllCharactersScreen(navController)}
 
         composable("comic") { ComicScreen(navController)}
         composable("series") { SeriesScreen(navController)  }
         composable("character") { CharacterScreen(navController) }
-        composable("comics_from_series"){ AllComicSeriesSec(navController = navController)}
-        composable("all_cs") { AllComicScreen(navController = navController) }
+        composable("comics_from_series"){ AllComicSeriesSec(navController)}
+        composable("all_cs") { AllComicScreen(navController) }
     }
 }
