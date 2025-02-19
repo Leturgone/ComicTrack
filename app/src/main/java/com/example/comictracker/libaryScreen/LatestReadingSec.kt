abf57290a370ee685aefb727bf04269ce5b86795
Leturgone <finekon221@gmail.com>
2025-02-19 23:02:32 +0300

    Сделано через функцию SeriesListCard

--- app/src/main/java/com/example/comictracker/libaryScreen/LatestReadingSec.kt
+++ app/src/main/java/com/example/comictracker/libaryScreen/LatestReadingSec.kt
@@ -1,5 +1,6 @@
 package com.example.comictracker.libaryScreen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,6 +19,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import coil3.compose.AsyncImage
+import com.example.comictracker.SeriesListCard
 import com.example.comictracker.data.ComicCover
 
 @Composable
@@ -33,26 +35,22 @@ fun LatestReadingSec(navController: NavHostController){
 
         Box(modifier = Modifier.fillMaxWidth(),contentAlignment = Alignment.TopEnd){
             Text(text = "See all",
-                color = MaterialTheme.colorScheme.primary, modifier = Modifier.padding(end = 15.dp, bottom = 12.dp))
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.clickable {
+                    navController.navigate("all_cs")
+                }.padding(end = 15.dp, bottom = 12.dp))
         }
         LazyRow{
             items(8){
                 //val latestComicCover  = latestReadingComicList[it]
-                var lastPaddingEnd = 0.dp
+                var lastPaddingEnd = 0
 //                if (it == latestReadingComicList.size - 1){
 //                    lastPaddingEnd = 16.dp
 //                }
-                Column(modifier = Modifier
-                    .padding(start = 16.dp, end = lastPaddingEnd)) {
-                    Card(modifier = Modifier
-                        .width(127.dp)
-                        .height(200.dp)) {
-                        AsyncImage(model = "http://i.annihil.us/u/prod/marvel/i/mg/9/c0/59dfdd3078b52.jpg"
-                            , contentDescription = "  current cover",modifier = Modifier
-                                .width(145.dp)
-                                .height(200.dp))
-                    }
-                    Text(text = "Date")
+                SeriesListCard(image = "http://i.annihil.us/u/prod/marvel/i/mg/9/c0/59dfdd3078b52.jpg",
+                    lastPaddingEnd =lastPaddingEnd, date = "Date" ) {
+                    navController.navigate("series")
+
                 }
 
             }
