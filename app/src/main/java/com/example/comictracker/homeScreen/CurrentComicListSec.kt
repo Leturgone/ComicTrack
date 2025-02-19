389d44961c4951f56a26fd8027286169cc038b41
Leturgone <finekon221@gmail.com>
2025-02-19 23:16:15 +0300

    Сделана навигация для HomeScreen

--- app/src/main/java/com/example/comictracker/homeScreen/CurrentComicListSec.kt
+++ app/src/main/java/com/example/comictracker/homeScreen/CurrentComicListSec.kt
@@ -1,5 +1,6 @@
 package com.example.comictracker.homeScreen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,6 +21,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import coil3.compose.AsyncImage
+import com.example.comictracker.SeriesListCard
 import com.example.comictracker.data.ComicCover
 
 
@@ -34,28 +36,21 @@ import com.example.comictracker.data.ComicCover
              modifier = Modifier.padding(16.dp))
          Box(modifier = Modifier.fillMaxWidth(),contentAlignment = Alignment.TopEnd){
              Text(text = "See all",
-                 color = MaterialTheme.colorScheme.primary, modifier = Modifier.padding(end = 15.dp, bottom = 12.dp))
+                 color = MaterialTheme.colorScheme.primary,
+                 modifier = Modifier.padding(end = 15.dp, bottom = 12.dp).clickable {
+                     navController.navigate("all_cs")
+                 })
          }
          LazyRow{
              items(8){
                  //val currentComicCover  = currentComicList[it]
-                 var lastPaddingEnd = 0.dp
+                 var lastPaddingEnd = 0
 //                if (it == currentComicList.size - 1){
 //                    lastPaddingEnd = 16.dp
 //                }
-                 Column(modifier = Modifier
-                     .padding(start = 16.dp, end = lastPaddingEnd)) {
-                     Card(modifier = Modifier
-                         .width(127.dp)
-                         .height(200.dp)) {
-                         Box(modifier = Modifier.fillMaxSize()){
-                         AsyncImage(model = "http://i.annihil.us/u/prod/marvel/i/mg/d/50/679ba099d9ebe.jpg"
-                             , contentDescription = "  current cover",modifier = Modifier
-                                 .width(145.dp)
-                                 .height(200.dp))
-                         }
-
-                     }
+                 SeriesListCard(image = "http://i.annihil.us/u/prod/marvel/i/mg/d/50/679ba099d9ebe.jpg",
+                     lastPaddingEnd = lastPaddingEnd) {
+                     navController.navigate("comic")
                  }
              }
          }
