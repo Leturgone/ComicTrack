5ffe4500ea444f3b768a27b1610a3b20c29cf15e
Leturgone <finekon221@gmail.com>
2025-02-20 12:08:57 +0300

    Сделана навигация с related

--- app/src/main/java/com/example/comictracker/aboutScreens/aboutSeries/RelatedSec.kt
+++ app/src/main/java/com/example/comictracker/aboutScreens/aboutSeries/RelatedSec.kt
@@ -1,14 +1,11 @@
 package com.example.comictracker.aboutScreens.aboutSeries
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,7 +15,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import coil3.compose.AsyncImage
+import com.example.comictracker.SeriesComicListCard
 import com.example.comictracker.data.ComicCover
 
 
@@ -38,28 +35,21 @@ fun RelatedSec(navController: NavHostController){
             modifier = Modifier.padding(16.dp))
         Box(modifier = Modifier.fillMaxWidth(),contentAlignment = Alignment.TopEnd){
             Text(text = "See all",
-                color = MaterialTheme.colorScheme.primary, modifier = Modifier.padding(end = 15.dp, bottom = 12.dp))
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.padding(end = 15.dp, bottom = 12.dp).clickable {
+                    navController.navigate("all_cs")
+                })
         }
         LazyRow{
             items(connectedComicList.size){
                 val currentComicCover  = connectedComicList[it]
-                var lastPaddingEnd = 0.dp
+                var lastPaddingEnd = 0
                 if (it == connectedComicList.size - 1){
-                    lastPaddingEnd = 16.dp
+                    lastPaddingEnd = 16
                 }
-                Column(modifier = Modifier
-                    .padding(start = 16.dp, end = lastPaddingEnd)) {
-                    Card(modifier = Modifier
-                        .width(127.dp)
-                        .height(200.dp)) {
-                        Box(modifier = Modifier.fillMaxSize()){
-                            AsyncImage(model = currentComicCover.imageUrl
-                                , contentDescription = "${currentComicCover.title}  current cover",modifier = Modifier
-                                    .width(145.dp)
-                                    .height(200.dp))
-                        }
-
-                    }
+                SeriesComicListCard(title = currentComicCover.title,
+                    image = currentComicCover.imageUrl, lastPaddingEnd = lastPaddingEnd ) {
+                    navController.navigate("series")
                 }
             }
         }
