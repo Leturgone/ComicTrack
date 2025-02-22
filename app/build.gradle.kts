f95edb48f7a4f9f1be9c25bbdd43c0860bdd8d3e
Leturgone <finekon221@gmail.com>
2025-02-22 20:32:37 +0300

    Добавлены зависимости для retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,4 +78,8 @@ dependencies {
 
     implementation("androidx.compose.material3:material3:1.4.0-alpha07")
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+
 }
