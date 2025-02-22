71b88fdaa6ac610a2f70392b87447baf0cb4a49d
Leturgone <finekon221@gmail.com>
2025-02-22 21:14:19 +0300

    Созданы заготовки для функций с апи

--- app/src/main/java/com/example/comictracker/data/api/MarvelComicApi.kt
+++ app/src/main/java/com/example/comictracker/data/api/MarvelComicApi.kt
@@ -2,4 +2,26 @@ package com.example.comictracker.data.api
 
 interface MarvelComicApi {
 
+
+    fun getSeries()
+
+    fun getComic()
+
+    fun getCharacter()
+
+    fun getComicListFromSeries()
+
+    fun getSeriesByCharacter()
+
+    fun getNewReleases()
+
+    fun getNewSeries()
+
+    fun getAllCharacters()
+
+    fun getComicsWithCharacter()
+
+    fun getConnectedComics()
+
+    fun searchSeries()
 }
