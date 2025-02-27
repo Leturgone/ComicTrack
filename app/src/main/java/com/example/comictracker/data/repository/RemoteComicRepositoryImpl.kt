39d7f907fd7906cadc76fc32116527a240624716
Leturgone <finekon221@gmail.com>
2025-02-27 15:50:16 +0300

    Созданы мапперы, потом переделать

--- app/src/main/java/com/example/comictracker/data/repository/RemoteComicRepositoryImpl.kt
+++ app/src/main/java/com/example/comictracker/data/repository/RemoteComicRepositoryImpl.kt
@@ -1,6 +1,10 @@
 package com.example.comictracker.data.repository
 
 import com.example.comictracker.data.api.MarvelComicApi
+import com.example.comictracker.data.api.dto.seriesDTO.Results as seriesResult
+import com.example.comictracker.data.api.dto.comicsDTO.Results as comicsResult
+import com.example.comictracker.data.api.dto.creatorsDTO.Results as creatorsResult
+import com.example.comictracker.data.api.dto.charactersDTO.Results as charactersResult
 import com.example.comictracker.domain.model.CharacterModel
 import com.example.comictracker.domain.model.ComicModel
 import com.example.comictracker.domain.model.CreatorModel
@@ -9,12 +13,90 @@ import com.example.comictracker.domain.repository.RemoteComicRepository
 import javax.inject.Inject
 
 class RemoteComicRepositoryImpl @Inject constructor(private val api: MarvelComicApi) :RemoteComicRepository {
+
+
+    //Мапперы
+    private fun seriesResult.toModel():SeriesModel{
+        return SeriesModel(
+            seriesId = this.id!!.toInt(),
+            title = this.title!!,
+            date = "${this.startYear} - ${this.endYear}",
+            desc = this.description!!,
+            image = "${this.urls[0].url}${this.urls[0].type}",
+            comics = this.comics!!.items.map {
+                it.resourceURI!!.toInt()
+            },
+            creators = this.creators!!.items.map {
+                it.resourceURI!!.toInt()
+            },
+            characters = this.characters!!.items.map {
+                it.resourceURI!!.toInt()
+            },
+            connectedSeries = listOf(
+                this.next?.resourceURI?.toInt(),
+                this.previous?.resourceURI?.toInt()),
+            readMark = ""
+        )
+    }
+
+    private fun comicsResult.toModel():ComicModel{
+        return ComicModel(
+            comicId = this.id.toInt(),
+            title =  this.title,
+            number =  this.issueNumber,
+            image = "${this.urls[0].url}${this.urls[0].type}",
+            seriesId =this.series!!.resourceURI!!.toInt(),
+            seriesTitle = this.series!!.name!!,
+            date = this.dates[0].date.toString(),
+            creators = this.creators!!.items.map {
+                it.resourceURI!!.toInt()
+            },
+            characters = this.characters!!.items.map {
+                it.resourceURI!!.toInt()
+            },
+            readMark = ""
+        )
+    }
+
+    private fun creatorsResult.toModel():CreatorModel{
+        return CreatorModel(
+            creatorId = this.id!!.toInt(),
+            name = this.fullName!!,
+            image = "${this.urls[0].url}${this.urls[0].type}",
+            role = this.suffix!!
+        )
+    }
+
+    private fun charactersResult.toModel():CharacterModel{
+        return CharacterModel(
+            characterId = this.id.toInt(),
+            name = this.name,
+            image = "${this.urls[0].url}${this.urls[0].type}",
+            desc = this.description,
+            series = this.series.items.map {
+                it.resourceURI!!.toInt()
+            }
+        )
+    }
+
+
+
     override suspend fun getSeriesLastReleasesById(id: Int): List<ComicModel> {
-        TODO("Not yet implemented")
+        val seriesLastReleases = mutableListOf<ComicModel>()
+        api.getSeriesLastReleasesById(series = id.toString()).data?.results?.forEach {
+            results ->  seriesLastReleases.add(results.toModel())
+        }
+        return seriesLastReleases
     }
 
     override suspend fun getCharacterSeries(characterId: Int): List<SeriesModel> {
-        TODO("Not yet implemented")
+        val characterSeries = mutableListOf<SeriesModel>()
+        api.getCharacterSeries(characterId).data?.results?.forEach { result ->
+            characterSeries.add(
+                result.toModel()
+            )
+        }
+        return characterSeries
     }
 
     override suspend fun getAllSeries(): List<SeriesModel> {
