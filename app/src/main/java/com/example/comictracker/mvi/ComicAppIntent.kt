1a4d75d31360ee77b21a50bc5154f698ad978125
Leturgone <finekon221@gmail.com>
2025-03-02 22:27:01 +0300

    Изменены параметры интента загрузки экрана персонажа

--- app/src/main/java/com/example/comictracker/mvi/ComicAppIntent.kt
+++ app/src/main/java/com/example/comictracker/mvi/ComicAppIntent.kt
@@ -10,7 +10,7 @@ sealed class ComicAppIntent {
     data class LoadSeriesScreen(val seriesId:Int): ComicAppIntent()
     data class LoadComicFromSeriesScreen(val seriesId: Int):ComicAppIntent()
     data class LoadComicScreen(val comicId:Int):ComicAppIntent()
-    data class LoadCharacterScreen(val character:CharacterModel):ComicAppIntent()
+    data class LoadCharacterScreen(val characterId:Int):ComicAppIntent()
     data class MarkAsReadSeries(val seriesId:Int): ComicAppIntent()
     data class MarkAsWillBeReadSeries(val seriesId:Int): ComicAppIntent()
     data class MarkAsCurrentlyReadingSeries(val seriesId:Int): ComicAppIntent()
