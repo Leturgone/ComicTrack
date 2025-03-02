2713a7a99b5ed6d18dd0c61d2333f5217b660f1f
Leturgone <finekon221@gmail.com>
2025-03-02 16:58:22 +0300

    Добавлены состояния

--- app/src/main/java/com/example/comictracker/mvi/ComicAppState.kt
+++ app/src/main/java/com/example/comictracker/mvi/ComicAppState.kt
@@ -25,17 +25,15 @@ sealed class ComicAppState {
     ): ComicAppState()
 
     data class AboutComicScreenState(
-        val comic: DataState<ComicModel> = DataState.Loading,
-        val creatorList: DataState<List<CreatorModel>> = DataState.Loading,
-        val characterList: DataState<List<CharacterModel>> = DataState.Loading
+        val dataState: DataState<AboutComicScreenData> = DataState.Loading
     ): ComicAppState()
 
     data class AboutSeriesScreenState(
-        val series: DataState<SeriesModel> = DataState.Loading,
-        val comicList: DataState<List<ComicModel>> = DataState.Loading,
-        val creatorList: DataState<List<CreatorModel>> = DataState.Loading,
-        val characterList: DataState<List<CharacterModel>> = DataState.Loading,
-        val connectedSeriesList: DataState<List<SeriesModel>> = DataState.Loading
+        val dataState: DataState<AboutSeriesScreenData> = DataState.Loading
+    ): ComicAppState()
+
+    data class AllComicSeriesScreenState(
+        val comicFromSeriesList:DataState<List<ComicModel>> = DataState.Loading,
     ): ComicAppState()
 
     data class AboutCharacterScreenState(
@@ -56,3 +54,48 @@ sealed class ComicAppState {
         val series: DataState<List<SeriesModel>> = DataState.Loading
     ): ComicAppState()
 }
+
+
+
+
+data class HomeScreenData(
+    val newReleasesList: List<ComicModel> = emptyList(),
+    val continueReadingList: List<ComicModel> = emptyList()
+)
+data class SearchScreenData(
+    val mayLikeList: List<SeriesModel> = emptyList(),
+    val discoverList: List<SeriesModel> = emptyList(),
+    val charactersSec: List<CharacterModel> = emptyList()
+): ComicAppState()
+
+data class MyLibraryScreenData(
+    val statistics: StatisticsforAll = StatisticsforAll(0,0,0),
+    val favoritesList: List<SeriesModel> = emptyList(),
+    val currentlyReadingList: List<SeriesModel>  = emptyList(),
+    val lastUpdates: List<ComicModel> = emptyList()
+): ComicAppState()
+
+data class AboutComicScreenData(
+    val comic: ComicModel? = null,
+    val creatorList: List<CreatorModel> = emptyList(),
+    val characterList: List<CharacterModel> = emptyList()
+)
+
+data class AboutSeriesScreenData(
+    val series: SeriesModel? = null,
+    val comicList: List<ComicModel> = emptyList(),
+    val creatorList: List<CreatorModel> = emptyList(),
+    val characterList: List<CharacterModel> = emptyList(),
+    val connectedSeriesList: List<SeriesModel> = emptyList()
+)
+
+data class AboutCharacterScreenData(
+    val character: CharacterModel? = null,
+    val series: List<SeriesModel> = emptyList()
+)
+
+
+data class SearchResultScreenData(
+    val character: List<CharacterModel> = emptyList(),
+    val series: List<SeriesModel> = emptyList()
+)
