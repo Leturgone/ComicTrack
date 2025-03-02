3cfe627156382d06c0b22a457766c47e4fd6766f
Leturgone <finekon221@gmail.com>
2025-03-02 16:56:02 +0300

    Подключено API ко экрану со всеми выпусками

--- app/src/main/java/com/example/comictracker/ui/screens/aboutScreens/aboutSeries/AllComicSeriesSec.kt
+++ app/src/main/java/com/example/comictracker/ui/screens/aboutScreens/aboutSeries/AllComicSeriesSec.kt
@@ -3,37 +3,67 @@ package com.example.comictracker.ui.screens.aboutScreens.aboutSeries
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.comictracker.data.model.ComicCover
+import com.example.comictracker.domain.model.ComicModel
+import com.example.comictracker.mvi.ComicAppIntent
+import com.example.comictracker.mvi.ComicAppState
+import com.example.comictracker.mvi.DataState
+import com.example.comictracker.viewmodel.ComicViewModel
 
 @Composable
-fun AllComicSeriesSec(navController: NavHostController){
-    val comics = listOf(
-        ComicCover("Spider-Man Noir #1","http://i.annihil.us/u/prod/marvel/i/mg/5/e0/5bc77a942112a.jpg",null),
-        ComicCover("Spider-Man Noir #2","http://i.annihil.us/u/prod/marvel/i/mg/5/e0/5bc77a942112a.jpg",null),
-        ComicCover("Spider-Man Noir #3","http://i.annihil.us/u/prod/marvel/i/mg/5/e0/5bc77a942112a.jpg",null),
-        ComicCover("Spider-Man Noir #4","http://i.annihil.us/u/prod/marvel/i/mg/5/e0/5bc77a942112a.jpg",null),
-        ComicCover("Spider-Man Noir #5","http://i.annihil.us/u/prod/marvel/i/mg/5/e0/5bc77a942112a.jpg",null),
-        ComicCover("Spider-Man Noir #6","http://i.annihil.us/u/prod/marvel/i/mg/5/e0/5bc77a942112a.jpg",null),
-        ComicCover("Spider-Man Noir #7","http://i.annihil.us/u/prod/marvel/i/mg/5/e0/5bc77a942112a.jpg",null)
-    )
+fun AllComicSeriesSec(seriesId:Int,
+                      viewModel: ComicViewModel = hiltViewModel(),
+                      navController: NavHostController){
+
+    val uiState by viewModel.state.collectAsState()
+    LaunchedEffect(key1 = seriesId) {
+        viewModel.processIntent(ComicAppIntent.LoadComicFromSeriesScreen(seriesId))
+    }
+
+
     Column {
         Text(text = "All Comics ",
             fontSize = 24.sp,
             color = MaterialTheme.colorScheme.onBackground,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(16.dp))
-        LazyColumn{
-            items(comics.size){
-                ComicFromSeriesCard(navController)
+
+        uiState.let { state ->
+            when(state){
+                is ComicAppState.AllComicSeriesScreenState -> {
+                    when(state.comicFromSeriesList){
+                        is DataState.Error -> TODO()
+                        DataState.Loading -> CircularProgressIndicator()
+                        is DataState.Success -> {
+                            val comics = state.comicFromSeriesList.result
+                            LazyColumn{
+                                items(comics.size){
+                                    val comic = comics[it]
+                                    ComicFromSeriesCard(comic,navController)
+                                }
+                            }
+                        }
+                    }
+                }
+
+                else -> {
+                    CircularProgressIndicator()
+                }
             }
         }
+
     }
 }
