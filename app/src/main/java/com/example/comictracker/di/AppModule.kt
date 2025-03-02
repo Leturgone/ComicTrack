96ec29adeef1ab597f35850ba80918df53ea1f39
Leturgone <finekon221@gmail.com>
2025-03-02 22:25:31 +0300

    Добавлено логгирование HTTP

--- app/src/main/java/com/example/comictracker/di/AppModule.kt
+++ app/src/main/java/com/example/comictracker/di/AppModule.kt
@@ -7,8 +7,11 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -17,9 +20,19 @@ class AppModule {
     @Provides
     @Singleton
     fun provideMarvelComicApi():MarvelComicApi{
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.HEADERS)
+
+        val client = OkHttpClient.Builder()
+            .connectTimeout(60, TimeUnit.SECONDS) // Увеличение времени ожидания соединения
+            .readTimeout(60, TimeUnit.SECONDS)    // Увеличение времени ожидания чтения данных
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .addInterceptor(logging)
+            .build()
         return Retrofit
             .Builder().baseUrl("https://gateway.marvel.com/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
             .create(MarvelComicApi::class.java)
     }
