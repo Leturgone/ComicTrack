72f4e444507d6f5c3ef77b48d5d26b877db649c9
Leturgone <finekon221@gmail.com>
2025-03-02 14:30:28 +0300

    Добавлен параметр списка

--- app/src/main/java/com/example/comictracker/ui/screens/libaryScreen/FavoriteSec.kt
+++ app/src/main/java/com/example/comictracker/ui/screens/libaryScreen/FavoriteSec.kt
@@ -6,17 +6,23 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.observe
 import androidx.navigation.NavHostController
 import com.example.comictracker.ui.screens.SeriesComicListCard
 import com.example.comictracker.data.model.ComicCover
+import com.example.comictracker.viewmodel.ComicViewModel
 
 @Composable
-fun FavoriteSec(navController: NavHostController){
-    var favoritesComicList: List<ComicCover> = listOf()
+fun FavoriteSec(navController: NavHostController,
+                viewModel: ComicViewModel = hiltViewModel()){
+    var favoritesComicList: List<Int> = listOf()
 
     Column {
         Text(text = "Favorites",
@@ -27,12 +33,12 @@ fun FavoriteSec(navController: NavHostController){
 
         LazyRow{
             items(8){
-                //val favoriteComicCover  = currentReadingComicList[it]
+                val favoriteComicId  = favoritesComicList[it]
                 var lastPaddingEnd = 0
-//                if (it == currentReadingComicList.size - 1){
-//                    lastPaddingEnd = 16.dp
-//                }
-                SeriesComicListCard(title = "comic title",
+                if (it == favoritesComicList.size - 1){
+                    lastPaddingEnd = 16
+                }
+                SeriesComicListCard(title = "${favoritesComicList[it]} comic title",
                     image = "http://i.annihil.us/u/prod/marvel/i/mg/9/c0/59dfdd3078b52.jpg",
                     lastPaddingEnd =lastPaddingEnd ) {
                     navController.navigate("series")
