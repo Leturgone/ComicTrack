8572fb25ab1598677ddee5e038cf8f20abce845c
Leturgone <finekon221@gmail.com>
2025-03-03 21:01:05 +0300

    Сделано состояние в экране комикса

--- app/src/main/java/com/example/comictracker/ui/screens/aboutScreens/aboutComic/ComicSceeen.kt
+++ app/src/main/java/com/example/comictracker/ui/screens/aboutScreens/aboutComic/ComicSceeen.kt
@@ -3,15 +3,50 @@ package com.example.comictracker.ui.screens.aboutScreens.aboutComic
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.comictracker.mvi.ComicAppIntent
+import com.example.comictracker.mvi.ComicAppState
+import com.example.comictracker.mvi.DataState
 import com.example.comictracker.ui.screens.aboutScreens.AboutCreatorsAndCharactersSec
+import com.example.comictracker.viewmodel.ComicViewModel
+
 @Composable
-fun  ComicScreen(navController1: Int, navController: NavHostController){
-    Column(Modifier.verticalScroll(rememberScrollState())) {
-        AboutComicSec(navController)
-        UsersComicMarkSec()
-        AboutCreatorsAndCharactersSec(emptyList(), emptyList(), navController)
+fun  ComicScreen(comicId: Int,
+                 navController: NavHostController,
+                 viewModel:ComicViewModel = hiltViewModel()){
+
+    val uiState by viewModel.state.collectAsState()
+
+    LaunchedEffect(key1 = comicId) {
+        viewModel.processIntent(ComicAppIntent.LoadComicScreen(comicId))
     }
+
+    uiState.let {state ->
+        when(state){
+            is ComicAppState.AboutComicScreenState ->{
+                when(state.dataState){
+                    is DataState.Error -> TODO()
+                    DataState.Loading -> CircularProgressIndicator()
+                    is DataState.Success -> {
+                        Column(Modifier.verticalScroll(rememberScrollState())) {
+                            AboutComicSec(state.dataState.result.comic!!,navController)
+                            UsersComicMarkSec(comicId)
+                            AboutCreatorsAndCharactersSec(state.dataState.result.creatorList,
+                                state.dataState.result.characterList, navController)
+                        }
+                    }
+                }
+            }
+
+            else -> {CircularProgressIndicator()}
+        }
+    }
+    
 }
