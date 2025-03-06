72f8f1909b173acc29ba935c50512a4bbe1cef00
Leturgone <finekon221@gmail.com>
2025-03-06 12:38:35 +0300

    Добавлены новые поля статистикаи

--- app/src/main/java/com/example/comictracker/domain/model/StatisticsforAll.kt
+++ app/src/main/java/com/example/comictracker/domain/model/StatisticsforAll.kt
@@ -1,7 +1,9 @@
 package com.example.comictracker.domain.model
 
 data class StatisticsforAll(
-    val comicCount: Int,
+    val comicCount: Int ,
+    val comicCountThisYear: Int,
     val seriesCount: Int,
+    val seriesCountThisYear :Int,
     val readlistCount:Int
 )
