4e1c27a56cd47149989b2249ea40a236c0089b06
Leturgone <finekon221@gmail.com>
2025-03-10 13:55:34 +0300

    Удалены ненужные классы

--- app/src/main/java/com/example/comictracker/data/model/CharacterItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.comictracker.data.model
-
-data class CharacterItem(
-    val name: String,
-    val desc: String,
-    val image:String
-)
--- app/src/main/java/com/example/comictracker/data/model/ComicCover.kt
+++
@@ -1,7 +0,0 @@
-package com.example.comictracker.data.model
-
-data class ComicCover(
-    val title: String,
-    val imageUrl:String,
-    val date: String?
-)
