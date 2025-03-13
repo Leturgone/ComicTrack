645e603893bd8a7533476f0cd2ee38ed4d427e88
Leturgone <finekon221@gmail.com>
2025-03-13 20:48:47 +0300

    Сделан класс сущности серии

---
+++ app/src/main/java/com/example/comictracker/data/database/enteties/SeriesList.kt
@@ -0,0 +1,27 @@
+package com.example.comictracker.data.database.enteties
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "series_list",
+    foreignKeys = [
+        ForeignKey(
+            entity = SeriesEntity::class,
+            parentColumns = ["idSeries"],
+            childColumns = ["Series_idSeries"],
+            onUpdate = ForeignKey.CASCADE,
+            onDelete = ForeignKey.CASCADE
+        )
+    ])
+class SeriesList {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo("SeriesListItemId")
+    var id: Int = 0
+    @ColumnInfo("listType")
+    var listType: String = ""
+    @ColumnInfo(name = "Series_idSeries")
+    var Series_idSeries:Int = 0
+
+}
