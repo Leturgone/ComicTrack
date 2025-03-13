3b7dc6558db0dbbf8847a42a1e88d1ffbbfa4af3
Leturgone <finekon221@gmail.com>
2025-03-13 20:48:52 +0300

    Сделан класс сущности серии

---
+++ app/src/main/java/com/example/comictracker/data/database/enteties/SeriesEntity.kt
@@ -0,0 +1,18 @@
+package com.example.comictracker.data.database.enteties
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "series")
+class SeriesEntity {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo("idSeries")
+    var id:Int = 0
+    @ColumnInfo("seriesApiId")
+    var seriesApiId:Int = 0
+    @ColumnInfo("mark")
+    var mark:String  ="unread"
+    @ColumnInfo("lastReadId")
+    var lastReadId:Int = 0
+}
