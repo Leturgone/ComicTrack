aabdfb55fe9efd7b906688360f493b0c26b51299
Leturgone <finekon221@gmail.com>
2025-04-11 15:56:34 +0300

    сделано дао для таблицы с комиксами

---
+++ app/src/main/java/com/example/comictracker/data/database/dao/ComicsDao.kt
@@ -0,0 +1,28 @@
+package com.example.comictracker.data.database.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.comictracker.data.database.enteties.ComicsEntity
+
+@Dao
+interface ComicsDao {
+    @Query("SELECT * FROM comics")
+    fun getComics(): LiveData<List<ComicsEntity>>
+
+    @Query("SELECT * FROM comics WHERE idComics=:id")
+    fun getComicById(id:Int)
+
+    @Query("SELECT mark FROM comics WHERE comicApiId =:apiId")
+    fun getComicMark(apiId:Int): LiveData<String>
+
+    @Insert
+    fun addComic(comicsEntity: ComicsEntity)
+
+    @Query("DELETE FROM comics WHERE comicApiId = :apiId")
+    fun removeComic(apiId:Int)
+
+}
