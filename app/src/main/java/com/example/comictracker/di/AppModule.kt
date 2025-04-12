95c6991fc56c052cfe75a9abd975fc10f6409c91
Leturgone <finekon221@gmail.com>
2025-04-12 21:42:49 +0300

    Сделан DI для бд

--- app/src/main/java/com/example/comictracker/di/AppModule.kt
+++ app/src/main/java/com/example/comictracker/di/AppModule.kt
@@ -1,11 +1,20 @@
 package com.example.comictracker.di
 
+import android.content.Context
+import androidx.room.Room
 import com.example.comictracker.data.api.MarvelComicApi
+import com.example.comictracker.data.database.ComicTrackerDatabase
+import com.example.comictracker.data.database.dao.ComicsDao
+import com.example.comictracker.data.database.dao.SeriesDao
+import com.example.comictracker.data.database.dao.SeriesListDao
+import com.example.comictracker.data.repository.LocalComicRepositoryImpl
 import com.example.comictracker.data.repository.RemoteComicRepositoryImpl
+import com.example.comictracker.domain.repository.LocalComicRepository
 import com.example.comictracker.domain.repository.RemoteComicRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -36,10 +45,48 @@ class AppModule {
             .build()
             .create(MarvelComicApi::class.java)
     }
+
     @Provides
     @Singleton
     fun provideRemoteRepository(api: MarvelComicApi):RemoteComicRepository{
         return RemoteComicRepositoryImpl(api)
     }
+
+    @Provides
+    @Singleton
+    fun provideComicTrackerDatabase(@ApplicationContext context: Context): ComicTrackerDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            ComicTrackerDatabase::class.java,
+            "comic_track_database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideComicsDao(db:ComicTrackerDatabase): ComicsDao{
+        return db.comicsDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideSeriesDao(db:ComicTrackerDatabase): SeriesDao{
+        return db.seriesDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideSeriesListDao(db:ComicTrackerDatabase): SeriesListDao{
+        return db.seriesListDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideLocalRepository(comicsDao: ComicsDao, seriesDao: SeriesDao,
+                               seriesListDao: SeriesListDao): LocalComicRepository {
+        return LocalComicRepositoryImpl(comicsDao, seriesDao, seriesListDao)
+    }
+
+
 
 }
