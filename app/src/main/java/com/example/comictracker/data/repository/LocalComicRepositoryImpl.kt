a61672504b0f691556f754e88701b18770c06af9
Leturgone <finekon221@gmail.com>
2025-04-12 20:01:15 +0300

    Начат репозиторий

--- app/src/main/java/com/example/comictracker/data/repository/LocalComicRepositoryImpl.kt
+++ app/src/main/java/com/example/comictracker/data/repository/LocalComicRepositoryImpl.kt
@@ -1,6 +1,14 @@
 package com.example.comictracker.data.repository
 
+import com.example.comictracker.data.database.dao.ComicsDao
+import com.example.comictracker.data.database.dao.SeriesDao
+import com.example.comictracker.data.database.dao.SeriesListDao
 import com.example.comictracker.domain.repository.LocalComicRepository
 
-class LocalComicRepositoryImpl: LocalComicRepository {
+class LocalComicRepositoryImpl(
+    private val comicsDao: ComicsDao,
+    private val seriesDao: SeriesDao,
+    private val seriesListDao: SeriesListDao
+): LocalComicRepository {
+
 }
