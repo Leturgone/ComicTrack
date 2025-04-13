1b31d9061d12304c4a72cc0d0a3eee9678044dcb
Leturgone <finekon221@gmail.com>
2025-04-13 23:17:45 +0300

    Создана БД

--- app/src/main/java/com/example/comictracker/data/database/ComicTrackerDatabase.kt
+++ app/src/main/java/com/example/comictracker/data/database/ComicTrackerDatabase.kt
@@ -7,9 +7,10 @@ import com.example.comictracker.data.database.dao.SeriesDao
 import com.example.comictracker.data.database.dao.SeriesListDao
 import com.example.comictracker.data.database.enteties.ComicsEntity
 import com.example.comictracker.data.database.enteties.SeriesEntity
+import com.example.comictracker.data.database.enteties.SeriesListEntity
 
 
-@Database(entities = [ComicsEntity::class, ComicsEntity::class, SeriesEntity::class], version = 1)
+@Database(entities = [ComicsEntity::class, SeriesEntity::class,SeriesListEntity::class], version = 1)
 abstract class ComicTrackerDatabase :RoomDatabase(){
     abstract fun comicsDao():ComicsDao
 
