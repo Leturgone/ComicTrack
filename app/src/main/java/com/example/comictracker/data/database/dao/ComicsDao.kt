225594f10dfa0d03c11bec5b9fbef95605c7860b
Leturgone <finekon221@gmail.com>
2025-04-15 03:44:59 +0300

    Добавлены методы получения по api id

--- app/src/main/java/com/example/comictracker/data/database/dao/ComicsDao.kt
+++ app/src/main/java/com/example/comictracker/data/database/dao/ComicsDao.kt
@@ -5,17 +5,27 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.comictracker.data.database.enteties.ComicsEntity
+import com.example.comictracker.data.database.enteties.SeriesEntity
 
 @Dao
 interface ComicsDao {
     @Query("SELECT * FROM comics")
     fun getComics(): LiveData<List<ComicsEntity>>
 
+    @Query("SELECT COUNT(*) FROM comics WHERE mark = 'read'")
+    fun getComicsCount(): Int
+
+    @Query("SELECT * FROM comics WHERE comicApiId=:apiId")
+    fun getComicByApiId(apiId:Int): ComicsEntity?
+
+    @Query("SELECT comicApiId FROM comics WHERE mark = 'read'")
+    fun getReadComicApiIds():List<Int>
+    
     @Query("SELECT * FROM comics WHERE idComics=:id")
-    fun getComicById(id:Int)
+    fun getComicById(id:Int):ComicsEntity
 
     @Query("SELECT mark FROM comics WHERE comicApiId =:apiId")
-    fun getComicMark(apiId:Int): LiveData<String>
+    fun getComicMark(apiId:Int): String?
 
     @Insert
     fun addComic(comicsEntity: ComicsEntity)
