079ff3f63749d82bbc033f8612a851f211c5cf5b
Leturgone <finekon221@gmail.com>
2025-04-15 03:46:39 +0300

    Создан локальный репозиторий

--- app/src/main/java/com/example/comictracker/domain/repository/LocalComicRepository.kt
+++ app/src/main/java/com/example/comictracker/domain/repository/LocalComicRepository.kt
@@ -1,4 +1,37 @@
 package com.example.comictracker.domain.repository
 
+import com.example.comictracker.domain.model.StatisticsforAll
+
 interface LocalComicRepository {
+    suspend fun loadCurrentReadIds():List<Int>
+    suspend fun loadNextReadComicIds():List<Int>
+    suspend fun loadLastComicIds():List<Int>
+    suspend fun loadAllReadComicIds():List<Int>
+
+    suspend fun loadAllReadSeriesIds():List<Int>
+
+    suspend fun loadWillBeReadIds():List<Int>
+
+    suspend fun loadStatistics():StatisticsforAll
+
+    suspend fun loadFavoritesIds():List<Int>
+
+    suspend fun loadComicMark(apiId: Int):String
+
+    suspend fun loadSeriesMark(apiId:Int):String
+
+    suspend fun markSeriesRead(apiId: Int):Boolean
+
+    suspend fun markComicRead(apiId: Int,seriesApiId:Int):Boolean
+
+    suspend fun addSeriesToFavorite(apiId: Int):Boolean
+
+    suspend fun addSeriesToCurrentlyRead(apiId: Int):Boolean
+
+    suspend fun addSeriesToWillBeRead(apiId: Int):Boolean
+
+    suspend fun markComicUnread(apiId: Int):Boolean
+    
+    suspend fun markSeriesUnread(apiId: Int):Boolean
+
 }
