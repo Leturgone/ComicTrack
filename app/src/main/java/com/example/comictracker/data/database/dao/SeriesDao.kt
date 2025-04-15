beb932173322858df6c85b1ce750109db4fb07df
Leturgone <finekon221@gmail.com>
2025-04-15 03:48:51 +0300

    В дао добавлены мтоды получения следуйщих и прошлых комиксов

--- app/src/main/java/com/example/comictracker/data/database/dao/SeriesDao.kt
+++ app/src/main/java/com/example/comictracker/data/database/dao/SeriesDao.kt
@@ -15,22 +15,19 @@ interface SeriesDao {
     fun addSeries(seriesEntity: SeriesEntity)
 
     @Query("SELECT * FROM series WHERE idSeries=:id")
-    fun getSeriesById(id:Int)
+    fun getSeriesById(id:Int):SeriesEntity
 
-    @Query("SELECT mark FROM series WHERE seriesApiId =:apiId")
-    fun getSeriesMark(apiId:Int): LiveData<String>
-
-    @Query("UPDATE series SET mark=:mark WHERE seriesApiId=:apiId")
-    fun setSeriesMark(apiId: Int,mark:String)
+    @Query("SELECT * FROM series WHERE seriesApiId=:apiId")
+    fun getSeriesByApiId(apiId:Int):SeriesEntity?
 
     @Query("DELETE FROM series where seriesApiId=:apiId")
     fun removeSeries(apiId: Int)
 
     @Query("SELECT lastReadId FROM series WHERE seriesApiId=:apiId")
-    fun getLastRead(apiId: Int):LiveData<Int>
+    fun getLastRead(apiId: Int):Int
 
     @Query("SELECT nextReadId FROM series WHERE seriesApiId=:apiId")
-    fun getNextRead(apiId:Int):LiveData<Int>
+    fun getNextRead(apiId:Int):Int
 
     @Query("UPDATE series SET lastReadId=:lastReadId WHERE seriesApiId=:apiId")
     fun setLastRead(apiId:Int,lastReadId:Int)
