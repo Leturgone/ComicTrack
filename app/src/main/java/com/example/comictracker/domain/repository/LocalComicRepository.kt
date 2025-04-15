70354add61cf2216b6fb6e86b816cd02d5fa16d2
Leturgone <finekon221@gmail.com>
2025-04-15 23:53:12 +0300

    Добавлен метод загрузки истории

--- app/src/main/java/com/example/comictracker/domain/repository/LocalComicRepository.kt
+++ app/src/main/java/com/example/comictracker/domain/repository/LocalComicRepository.kt
@@ -4,8 +4,13 @@ import com.example.comictracker.domain.model.StatisticsforAll
 
 interface LocalComicRepository {
     suspend fun loadCurrentReadIds():List<Int>
+
     suspend fun loadNextReadComicIds():List<Int>
+
     suspend fun loadLastComicIds():List<Int>
+
+    suspend fun loadHistory():List<Int>
+
     suspend fun loadAllReadComicIds():List<Int>
 
     suspend fun loadAllReadSeriesIds():List<Int>
@@ -24,17 +29,17 @@ interface LocalComicRepository {
 
     suspend fun markSeriesRead(apiId: Int):Boolean
 
-    suspend fun markComicRead(apiId: Int,seriesApiId:Int):Boolean
+    suspend fun markComicRead(apiId: Int,seriesApiId:Int,nextComicApiId:Int?):Boolean
 
     suspend fun addSeriesToFavorite(apiId: Int):Boolean
 
     suspend fun removeSeriesFromFavorite(apiId: Int):Boolean
 
-    suspend fun addSeriesToCurrentlyRead(apiId: Int):Boolean
+    suspend fun addSeriesToCurrentlyRead(apiId: Int,firstIssueId:Int?):Boolean
 
     suspend fun addSeriesToWillBeRead(apiId: Int):Boolean
 
-    suspend fun markComicUnread(apiId: Int,seriesApiId:Int):Boolean
+    suspend fun markComicUnread(apiId: Int,seriesApiId:Int,prevComicApiId:Int?):Boolean
     
     suspend fun markSeriesUnread(apiId: Int):Boolean
 
