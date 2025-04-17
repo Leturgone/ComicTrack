e4aacc8fc141fdfd6fd917d88a6b827953eec670
Leturgone <finekon221@gmail.com>
2025-04-17 13:34:17 +0300

    Экран домашний сделан через ресурсы

--- app/src/main/java/com/example/comictracker/presentation/ui/screens/homeScreen/NewComicListSec.kt
+++ app/src/main/java/com/example/comictracker/presentation/ui/screens/homeScreen/NewComicListSec.kt
@@ -11,29 +11,32 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.comictracker.domain.model.ComicModel
 import com.example.comictracker.presentation.ui.screens.SeriesComicListCard
-
+import com.example.comictracker.R
 
 @Composable
 fun NewComicListSec(newComicList: List<ComicModel>, navController: NavHostController){
     Column {
-        Text(text = "New releases",
+        Text(text = stringResource(id = R.string.new_releases),
             fontSize = 24.sp,
             color = MaterialTheme.colorScheme.onBackground,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(16.dp))
 
         Box(modifier = Modifier.fillMaxWidth(),contentAlignment = Alignment.TopEnd){
-            Text(text = "See all",
+            Text(text = stringResource(id = R.string.see_all),
                 color = MaterialTheme.colorScheme.primary,
-                modifier = Modifier.padding(end = 15.dp, bottom = 12.dp).clickable {
-                    navController.navigate("all_cs/0/newComic/0")
-                })
+                modifier = Modifier
+                    .padding(end = 15.dp, bottom = 12.dp)
+                    .clickable {
+                        navController.navigate("all_cs/0/newComic/0")
+                    })
         }
         LazyRow{
             items(newComicList.size){
