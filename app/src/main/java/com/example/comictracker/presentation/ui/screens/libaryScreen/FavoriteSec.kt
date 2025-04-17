9c4fb4ac30bf1d0e7547f1ea44ebb53143090ffb
Leturgone <finekon221@gmail.com>
2025-04-17 13:33:59 +0300

    Экран библиотеки сделан через ресурсы

--- app/src/main/java/com/example/comictracker/presentation/ui/screens/libaryScreen/FavoriteSec.kt
+++ app/src/main/java/com/example/comictracker/presentation/ui/screens/libaryScreen/FavoriteSec.kt
@@ -7,18 +7,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.comictracker.presentation.ui.screens.SeriesComicListCard
 import com.example.comictracker.domain.model.SeriesModel
+import com.example.comictracker.R
 
 @Composable
 fun FavoriteSec(favoriteList: List<SeriesModel>, navController: NavHostController){
 
     Column {
-        Text(text = "Favorites",
+        Text(text = stringResource(id = R.string.favorites),
             fontSize = 24.sp,
             color = MaterialTheme.colorScheme.onBackground,
             fontWeight = FontWeight.Bold,
--- app/src/main/java/com/example/comictracker/presentation/ui/screens/libaryScreen/LatestReadingSec.kt
+++ app/src/main/java/com/example/comictracker/presentation/ui/screens/libaryScreen/LatestReadingSec.kt
@@ -11,29 +11,32 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.comictracker.domain.model.ComicModel
 import com.example.comictracker.presentation.ui.screens.SeriesComicListCard
-
+import com.example.comictracker.R
 @Composable
 fun LatestReadingSec(latestReadList: List<ComicModel>, navController: NavHostController){
 
     Column {
-        Text(text = "Last updates",
+        Text(text = stringResource(id = R.string.last_updates),
             fontSize = 24.sp,
             color = MaterialTheme.colorScheme.onBackground,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(16.dp))
 
         Box(modifier = Modifier.fillMaxWidth(),contentAlignment = Alignment.TopEnd){
-            Text(text = "See all",
+            Text(text = stringResource(id = R.string.see_all),
                 color = MaterialTheme.colorScheme.primary,
-                modifier = Modifier.clickable {
-                    navController.navigate("all_cs/0/lastComic/0")
-                }.padding(end = 15.dp, bottom = 12.dp))
+                modifier = Modifier
+                    .clickable {
+                        navController.navigate("all_cs/0/lastComic/0")
+                    }
+                    .padding(end = 15.dp, bottom = 12.dp))
         }
         LazyRow{
             items(latestReadList.size){
