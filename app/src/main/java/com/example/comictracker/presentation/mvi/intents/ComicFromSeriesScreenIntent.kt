a6522852d4bcc032f68cf56aa262a5690ef29262
Leturgone <finekon221@gmail.com>
2025-04-29 13:08:04 +0300

    добавлены интенты

--- app/src/main/java/com/example/comictracker/presentation/mvi/intents/ComicFromSeriesScreenIntent.kt
+++ app/src/main/java/com/example/comictracker/presentation/mvi/intents/ComicFromSeriesScreenIntent.kt
@@ -6,7 +6,7 @@ sealed class ComicFromSeriesScreenIntent {
 
     data class LoadComicFromSeriesScreen(val seriesId: Int, val loadCount: Int): ComicFromSeriesScreenIntent()
 
-    data class MarkAsReadComicInList(val comicId:Int,val seriesId: Int, val issueNumber:String,val loadedCount: Int?): ComicFromSeriesScreenIntent()
+    data class MarkAsReadComicInList(val comicId:Int,val seriesId: Int, val issueNumber:String,val loadedCount: Int): ComicFromSeriesScreenIntent()
 
     data class MarkAsUnreadComicInList(val comicId:Int,val seriesId: Int, val issueNumber:String,val loadedCount: Int): ComicFromSeriesScreenIntent()
 
