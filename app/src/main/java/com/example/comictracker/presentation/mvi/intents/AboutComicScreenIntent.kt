0aaa26ac2a43303ba76847b0b2a4748f69f4ff7b
Leturgone <finekon221@gmail.com>
2025-04-29 13:04:57 +0300

    Удалены лишние методы в интенте

--- app/src/main/java/com/example/comictracker/presentation/mvi/intents/AboutComicScreenIntent.kt
+++ app/src/main/java/com/example/comictracker/presentation/mvi/intents/AboutComicScreenIntent.kt
@@ -1,15 +1,9 @@
 package com.example.comictracker.presentation.mvi.intents
 
-import com.example.comictracker.presentation.mvi.ComicAppIntent
-
 sealed class AboutComicScreenIntent {
     data class LoadComicScreen(val comicId:Int): AboutComicScreenIntent()
 
     data class MarkAsReadComic(val comicId:Int,val seriesId: Int, val issueNumber:String): AboutComicScreenIntent()
 
     data class MarkAsUnreadComic(val comicId:Int,val seriesId: Int, val issueNumber:String): AboutComicScreenIntent()
-
-    data class MarkAsReadComicInList(val comicId:Int,val seriesId: Int, val issueNumber:String,val loadedCount: Int?): AboutComicScreenIntent()
-
-    data class MarkAsUnreadComicInList(val comicId:Int,val seriesId: Int, val issueNumber:String,val loadedCount: Int): AboutComicScreenIntent()
 }
