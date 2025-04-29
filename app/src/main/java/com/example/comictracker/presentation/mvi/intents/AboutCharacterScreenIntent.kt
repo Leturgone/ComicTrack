6868ce398209686ff4db6430d92b50159f9d6c05
Leturgone <finekon221@gmail.com>
2025-04-29 13:04:09 +0300

    Удаленор лишнее

--- app/src/main/java/com/example/comictracker/presentation/mvi/intents/AboutCharacterScreenIntent.kt
+++ app/src/main/java/com/example/comictracker/presentation/mvi/intents/AboutCharacterScreenIntent.kt
@@ -1,7 +1,5 @@
 package com.example.comictracker.presentation.mvi.intents
 
-import com.example.comictracker.presentation.mvi.ComicAppIntent
-
 sealed class AboutCharacterScreenIntent {
     data class LoadCharacterScreen(val characterId:Int): AboutCharacterScreenIntent()
 
