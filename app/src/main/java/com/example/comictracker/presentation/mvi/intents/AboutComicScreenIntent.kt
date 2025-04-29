353b5d378144f420002dcfd17a106f55876dec85
Leturgone <finekon221@gmail.com>
2025-04-29 10:04:01 +0300

    Сделаны  интенты для экрана о комиксе

--- app/src/main/java/com/example/comictracker/presentation/mvi/intents/AboutComicScreenIntent.kt
+++ app/src/main/java/com/example/comictracker/presentation/mvi/intents/AboutComicScreenIntent.kt
@@ -1,4 +1,15 @@
 package com.example.comictracker.presentation.mvi.intents
 
-class AboutComicScreenIntent {
+import com.example.comictracker.presentation.mvi.ComicAppIntent
+
+sealed class AboutComicScreenIntent {
+    data class LoadComicScreen(val comicId:Int): AboutComicScreenIntent()
+
+    data class MarkAsReadComic(val comicId:Int,val seriesId: Int, val issueNumber:String): AboutComicScreenIntent()
+
+    data class MarkAsUnreadComic(val comicId:Int,val seriesId: Int, val issueNumber:String): AboutComicScreenIntent()
+
+    data class MarkAsReadComicInList(val comicId:Int,val seriesId: Int, val issueNumber:String,val loadedCount: Int?): AboutComicScreenIntent()
+
+    data class MarkAsUnreadComicInList(val comicId:Int,val seriesId: Int, val issueNumber:String,val loadedCount: Int): AboutComicScreenIntent()
 }
