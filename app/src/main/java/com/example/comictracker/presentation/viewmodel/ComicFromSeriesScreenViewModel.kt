a9bed0dc5eb2fc3a1222bb7b8e5d8eb0f7b055d4
Leturgone <finekon221@gmail.com>
2025-04-29 13:08:30 +0300

    сделана вью модель для списка комиксов в серии

--- app/src/main/java/com/example/comictracker/presentation/viewmodel/ComicFromSeriesScreenViewModel.kt
+++ app/src/main/java/com/example/comictracker/presentation/viewmodel/ComicFromSeriesScreenViewModel.kt
@@ -1,4 +1,77 @@
 package com.example.comictracker.presentation.viewmodel
 
-class ComicFromSeriesScreenViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.comictracker.domain.repository.LocalComicRepository
+import com.example.comictracker.domain.repository.RemoteComicRepository
+import com.example.comictracker.presentation.mvi.ComicAppState
+import com.example.comictracker.presentation.mvi.DataState
+import com.example.comictracker.presentation.mvi.intents.ComicFromSeriesScreenIntent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@HiltViewModel
+class ComicFromSeriesScreenViewModel @Inject constructor(
+    private val remoteComicRepository: RemoteComicRepository,
+    private val localComicRepository: LocalComicRepository,
+): ViewModel(){
+
+    private val _state = MutableStateFlow<ComicAppState>(ComicAppState.HomeScreenState())
+    val state: StateFlow<ComicAppState> = _state
+
+    fun processIntent(intent:ComicFromSeriesScreenIntent){
+        when(intent){
+            is ComicFromSeriesScreenIntent.LoadComicFromSeriesScreen -> loadComicFromSeriesScreen(intent.seriesId,intent.loadCount)
+            is ComicFromSeriesScreenIntent.MarkAsReadComicInList -> markAsReadComicInList(intent.comicId,intent.seriesId, intent.issueNumber,intent.loadedCount)
+            is ComicFromSeriesScreenIntent.MarkAsUnreadComicInList -> markAsUnreadComicInList(intent.comicId,intent.seriesId, intent.issueNumber,intent.loadedCount)
+        }
+    }
+
+    private fun loadComicFromSeriesScreen(seriesId: Int,loadedCount: Int)  = viewModelScope.launch{
+        _state.value = ComicAppState.AllComicSeriesScreenState(DataState.Loading)
+        try {
+            withContext(Dispatchers.IO){
+                _state.emit(
+                    ComicAppState.AllComicSeriesScreenState(
+                        DataState.Success(
+                            remoteComicRepository.getComicsFromSeries(seriesId,loadedCount).map {
+                                val readMark = localComicRepository.loadComicMark(it.comicId)
+                                it.copy(readMark = readMark)
+                            })
+                    )
+
+                )
+            }
+        }catch (e:Exception){
+            _state.value = ComicAppState.AllComicSeriesScreenState(
+                DataState.Error("Error loading comic from this series : $e"))
+        }
+
+    }
+
+    private fun markAsReadComicInList(comicApiId: Int, seriesApiId: Int, number: String, loadedCount: Int) = viewModelScope.launch{
+        val nextComicId = async {
+            remoteComicRepository.getNextComicId(seriesApiId,number.toFloat().toInt())
+        }.await()
+
+        if (localComicRepository.markComicRead(comicApiId,seriesApiId,nextComicId)){
+            loadComicFromSeriesScreen(seriesApiId,loadedCount)
+        }
+    }
+
+    private fun markAsUnreadComicInList(comicApiId: Int, seriesApiId: Int, number: String, loadedCount: Int)  = viewModelScope.launch{
+        val prevComicId = async {
+            remoteComicRepository.getPreviousComicId(seriesApiId, number.toFloat().toInt())
+        }.await()
+
+        if (localComicRepository.markComicUnread(comicApiId,seriesApiId,prevComicId)){
+            loadComicFromSeriesScreen(seriesApiId,loadedCount)
+        }
+    }
 }
