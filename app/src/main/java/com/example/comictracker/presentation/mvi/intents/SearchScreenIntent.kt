fa4f0daea70fa0e7e6ed34c43b1c0cdb1a46d850
Leturgone <finekon221@gmail.com>
2025-04-29 10:05:25 +0300

    Перенесены  интенты для экрана поиска

--- app/src/main/java/com/example/comictracker/presentation/mvi/intents/SearchScreenIntent.kt
+++ app/src/main/java/com/example/comictracker/presentation/mvi/intents/SearchScreenIntent.kt
@@ -1,4 +1,6 @@
 package com.example.comictracker.presentation.mvi.intents
 
-class SearchScreenIntent {
+sealed class SearchScreenIntent {
+    data object LoadSearchScreen: SearchScreenIntent()
+    data class Search(val query:String): SearchScreenIntent()
 }
