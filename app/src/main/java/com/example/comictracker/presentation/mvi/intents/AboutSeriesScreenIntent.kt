7f089a3ae9267ff63f459898dcb0d2ed913b947e
Leturgone <finekon221@gmail.com>
2025-04-29 13:05:34 +0300

    Добавлены интенты

--- app/src/main/java/com/example/comictracker/presentation/mvi/intents/AboutSeriesScreenIntent.kt
+++ app/src/main/java/com/example/comictracker/presentation/mvi/intents/AboutSeriesScreenIntent.kt
@@ -1,18 +1,21 @@
 package com.example.comictracker.presentation.mvi.intents
 
-import com.example.comictracker.presentation.mvi.ComicAppIntent
-
 sealed class AboutSeriesScreenIntent {
-    data class LoadSeriesScreen(val seriesId:Int): AboutComicScreenIntent()
 
-    data class MarkAsReadSeries(val seriesId:Int): AboutComicScreenIntent()
+    data class LoadSeriesScreen(val seriesId:Int): AboutSeriesScreenIntent()
 
-    data class AddSeriesToFavorite(val seriesId:Int): AboutComicScreenIntent()
+    data class MarkAsReadSeries(val seriesId:Int): AboutSeriesScreenIntent()
 
-    data class RemoveSeriesFromFavorite(val seriesId:Int): AboutComicScreenIntent()
+    data class AddSeriesToFavorite(val seriesId:Int): AboutSeriesScreenIntent()
 
-    data class MarkAsWillBeReadSeries(val seriesId:Int): AboutComicScreenIntent()
+    data class RemoveSeriesFromFavorite(val seriesId:Int): AboutSeriesScreenIntent()
 
-    data class MarkAsCurrentlyReadingSeries(val seriesId:Int,val firstIssueId:Int?): AboutComicScreenIntent()
-    data class MarkAsUnreadSeries(val seriesId:Int): AboutComicScreenIntent()
+    data class MarkAsWillBeReadSeries(val seriesId:Int): AboutSeriesScreenIntent()
+
+    data class MarkAsCurrentlyReadingSeries(val seriesId:Int,val firstIssueId:Int?): AboutSeriesScreenIntent()
+
+    data class MarkAsUnreadSeries(val seriesId:Int): AboutSeriesScreenIntent()
+
+    data class MarkAsReadNextComic(val comicId:Int,val seriesId: Int, val issueNumber:String): AboutSeriesScreenIntent()
+
 }
