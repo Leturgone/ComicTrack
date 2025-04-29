9b82bc215e5f995696c12c56a712eaab839bba21
Leturgone <finekon221@gmail.com>
2025-04-29 10:04:15 +0300

    Перенесены  интенты для экрана о серии

--- app/src/main/java/com/example/comictracker/presentation/mvi/intents/AboutSeriesScreenIntent.kt
+++ app/src/main/java/com/example/comictracker/presentation/mvi/intents/AboutSeriesScreenIntent.kt
@@ -1,4 +1,18 @@
 package com.example.comictracker.presentation.mvi.intents
 
-class AboutSeriesScreenIntent {
+import com.example.comictracker.presentation.mvi.ComicAppIntent
+
+sealed class AboutSeriesScreenIntent {
+    data class LoadSeriesScreen(val seriesId:Int): AboutComicScreenIntent()
+
+    data class MarkAsReadSeries(val seriesId:Int): AboutComicScreenIntent()
+
+    data class AddSeriesToFavorite(val seriesId:Int): AboutComicScreenIntent()
+
+    data class RemoveSeriesFromFavorite(val seriesId:Int): AboutComicScreenIntent()
+
+    data class MarkAsWillBeReadSeries(val seriesId:Int): AboutComicScreenIntent()
+
+    data class MarkAsCurrentlyReadingSeries(val seriesId:Int,val firstIssueId:Int?): AboutComicScreenIntent()
+    data class MarkAsUnreadSeries(val seriesId:Int): AboutComicScreenIntent()
 }
