79e5b42f0ee214edbf13417138eb2294437aef1a
Leturgone <finekon221@gmail.com>
2025-04-29 16:34:14 +0300

    Удалено лишнее

--- app/src/main/java/com/example/comictracker/presentation/mvi/intents/AllScreenIntent.kt
+++ app/src/main/java/com/example/comictracker/presentation/mvi/intents/AllScreenIntent.kt
@@ -1,7 +1,5 @@
 package com.example.comictracker.presentation.mvi.intents
 
-import com.example.comictracker.presentation.mvi.ComicAppIntent
-
 sealed class AllScreenIntent {
     data class LoadAllScreen(val sourceId: Int, val sourceCat:String, val loadedCount: Int ): AllScreenIntent()
 
