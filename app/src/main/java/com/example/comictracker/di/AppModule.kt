b8036539235cf5636c7c7f4a432adb637e756977
Leturgone <finekon221@gmail.com>
2025-04-30 17:21:06 +0300

    Добавлены репозитории в модуль

--- app/src/main/java/com/example/comictracker/di/AppModule.kt
+++ app/src/main/java/com/example/comictracker/di/AppModule.kt
@@ -7,12 +7,14 @@ import com.example.comictracker.data.database.ComicTrackerDatabase
 import com.example.comictracker.data.database.dao.ComicsDao
 import com.example.comictracker.data.database.dao.SeriesDao
 import com.example.comictracker.data.database.dao.SeriesListDao
-import com.example.comictracker.data.repository.LocalComicRepositoryImpl
+import com.example.comictracker.data.repository.local.LocalReadRepositoryImpl
+import com.example.comictracker.data.repository.local.LocalWriteRepositoryImpl
 import com.example.comictracker.data.repository.remote.RemoteCharacterRepositoryImpl
 import com.example.comictracker.data.repository.remote.RemoteComicsRepositoryImpl
 import com.example.comictracker.data.repository.remote.RemoteCreatorsRepositoryImpl
 import com.example.comictracker.data.repository.remote.RemoteSeriesRepositoryImpl
-import com.example.comictracker.domain.repository.LocalComicRepository
+import com.example.comictracker.domain.repository.local.LocalReadRepository
+import com.example.comictracker.domain.repository.local.LocalWriteRepository
 import com.example.comictracker.domain.repository.remote.RemoteCharacterRepository
 import com.example.comictracker.domain.repository.remote.RemoteComicsRepository
 import com.example.comictracker.domain.repository.remote.RemoteCreatorsRepository
@@ -108,10 +110,18 @@ class AppModule {
 
     @Provides
     @Singleton
-    fun provideLocalRepository(comicsDao: ComicsDao, seriesDao: SeriesDao,
-                               seriesListDao: SeriesListDao): LocalComicRepository {
-        return LocalComicRepositoryImpl(comicsDao, seriesDao, seriesListDao)
+    fun provideLocalReadRepository(comicsDao: ComicsDao, seriesDao: SeriesDao,
+                               seriesListDao: SeriesListDao): LocalReadRepository {
+        return LocalReadRepositoryImpl(comicsDao, seriesDao, seriesListDao)
     }
+
+    @Provides
+    @Singleton
+    fun provideLocalWriteRepository(comicsDao: ComicsDao, seriesDao: SeriesDao,
+                                   seriesListDao: SeriesListDao): LocalWriteRepository {
+        return LocalWriteRepositoryImpl(comicsDao, seriesDao, seriesListDao)
+    }
+
 
 
 
