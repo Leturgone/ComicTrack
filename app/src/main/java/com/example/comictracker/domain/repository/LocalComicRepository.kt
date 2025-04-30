e586ffcc3ec979debd95e7427e02beaac87bfb78
Leturgone <finekon221@gmail.com>
2025-04-30 16:15:01 +0300

    Закоменчено

--- app/src/main/java/com/example/comictracker/domain/repository/LocalComicRepository.kt
+++ app/src/main/java/com/example/comictracker/domain/repository/LocalComicRepository.kt
@@ -2,46 +2,46 @@ package com.example.comictracker.domain.repository
 
 import com.example.comictracker.domain.model.StatisticsforAll
 
-interface LocalComicRepository {
-    suspend fun loadCurrentReadIds(offset:Int):List<Int>
-
-    suspend fun loadNextReadComicIds(offset:Int):List<Int>
-
-
-    suspend fun loadHistory(offset:Int):List<Int>
-
-    suspend fun loadAllReadComicIds(offset:Int):List<Int>
-
-    suspend fun loadAllReadSeriesIds(offset:Int):List<Int>
-
-    suspend fun loadWillBeReadIds(offset:Int):List<Int>
-
-    suspend fun loadStatistics():StatisticsforAll
-
-    suspend fun loadFavoritesIds():List<Int>
-
-    suspend fun loadComicMark(apiId: Int):String
-
-    suspend fun loadSeriesMark(apiId:Int):String
-
-    suspend fun loadSeriesFavoriteMark(apiId: Int):Boolean
-
-    suspend fun markSeriesRead(apiId: Int):Boolean
-
-    suspend fun markComicRead(apiId: Int,seriesApiId:Int,nextComicApiId:Int?):Boolean
-
-    suspend fun addSeriesToFavorite(apiId: Int):Boolean
-
-    suspend fun removeSeriesFromFavorite(apiId: Int):Boolean
-
-    suspend fun addSeriesToCurrentlyRead(apiId: Int,firstIssueId:Int?):Boolean
-
-    suspend fun addSeriesToWillBeRead(apiId: Int):Boolean
-
-    suspend fun markComicUnread(apiId: Int,seriesApiId:Int,prevComicApiId:Int?):Boolean
-    
-    suspend fun markSeriesUnread(apiId: Int):Boolean
-
-    suspend fun loadNextRead(apiId: Int):Int?
-
-}
+//interface LocalComicRepository {
+//    suspend fun loadCurrentReadIds(offset:Int):List<Int>
+//
+//    suspend fun loadNextReadComicIds(offset:Int):List<Int>
+//
+//
+//    suspend fun loadHistory(offset:Int):List<Int>
+//
+//    suspend fun loadAllReadComicIds(offset:Int):List<Int>
+//
+//    suspend fun loadAllReadSeriesIds(offset:Int):List<Int>
+//
+//    suspend fun loadWillBeReadIds(offset:Int):List<Int>
+//
+//    suspend fun loadStatistics():StatisticsforAll
+//
+//    suspend fun loadFavoritesIds():List<Int>
+//
+//    suspend fun loadComicMark(apiId: Int):String
+//
+//    suspend fun loadSeriesMark(apiId:Int):String
+//
+//    suspend fun loadSeriesFavoriteMark(apiId: Int):Boolean
+//
+//    suspend fun markSeriesRead(apiId: Int):Boolean
+//
+//    suspend fun markComicRead(apiId: Int,seriesApiId:Int,nextComicApiId:Int?):Boolean
+//
+//    suspend fun addSeriesToFavorite(apiId: Int):Boolean
+//
+//    suspend fun removeSeriesFromFavorite(apiId: Int):Boolean
+//
+//    suspend fun addSeriesToCurrentlyRead(apiId: Int,firstIssueId:Int?):Boolean
+//
+//    suspend fun addSeriesToWillBeRead(apiId: Int):Boolean
+//
+//    suspend fun markComicUnread(apiId: Int,seriesApiId:Int,prevComicApiId:Int?):Boolean
+//
+//    suspend fun markSeriesUnread(apiId: Int):Boolean
+//
+//    suspend fun loadNextRead(apiId: Int):Int?
+//
+//}
