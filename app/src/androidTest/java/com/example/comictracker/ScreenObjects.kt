ba5c12f5f398b87eb8e8b6b772137b225e16f58c
Leturgone <finekon221@gmail.com>
2025-05-04 21:17:11 +0300

    Добавлены объекты

--- app/src/androidTest/java/com/example/comictracker/ScreenObjects.kt
+++ app/src/androidTest/java/com/example/comictracker/ScreenObjects.kt
@@ -42,7 +42,7 @@ object SearchScreenTestObj{
     val seeAllDiscoverTemplate = (hasText("See all")) and hasClickAction() and hasTestTag("seeAllDiscover")
     val discoverList = hasContentDescription("${seriesExample.title}  current cover")
     val charactersTemplate = (hasText("Characters")) and hasNoClickAction()
-    val seeAllCharactersTemplate = (hasText("See all")) and hasClickAction() and hasTestTag("seeAllCharacters")
+    val seeAllCharactersTemplate = hasTestTag("seeAllCharacters")
     val characterList = hasContentDescription("${characterExample.name} character")
 }
 
@@ -52,11 +52,14 @@ object LibraryScreenTestObj{
     val seriesTemplate = (hasText("Series")) and hasClickAction()
     val readlistTemplate = (hasText("Readlist")) and hasClickAction()
     val favoriteTemplate = (hasText("Favorites")) and hasNoClickAction()
-    val favoritesList = hasContentDescription("  current cover")
+    val favoritesList = hasContentDescription("${secondSeriesExample.title}  current cover")
     val curReadTemplate = (hasText("Currently reading")) and hasNoClickAction()
-    val curReadList = hasContentDescription("  current cover")
+    val curReadList = hasContentDescription("${seriesExample.title}  current cover")
     val lastUpdatesTemplate = (hasText("Last updates")) and hasNoClickAction()
-    val lastUpdatesList = hasContentDescription("  current cover")
+    val lastUpdatesList = hasContentDescription("${comicExample.title}  current cover")
+    val seeAllCurrentTemplate =  (hasText("See all")) and hasClickAction() and hasTestTag("seeAllCurrent")
+    val seeAllLastTemplate =  (hasText("See all")) and hasClickAction() and hasTestTag("seeAllLast")
+
 
 
 }
