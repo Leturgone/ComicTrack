079af0702ba7dfaa1274671bac84b42472304ce1
Leturgone <finekon221@gmail.com>
2025-05-06 00:15:01 +0300

    Разделены интенты

--- app/src/main/java/com/example/comictracker/presentation/mvi/intents/AllScreenIntent.kt
+++ app/src/main/java/com/example/comictracker/presentation/mvi/intents/AllScreenIntent.kt
@@ -1,8 +1,6 @@
 package com.example.comictracker.presentation.mvi.intents
 
 sealed class AllScreenIntent {
-    data class LoadAllScreen(val sourceId: Int, val sourceCat:String, val loadedCount: Int ): AllScreenIntent()
-
     data class LoadAllCharactersScreen(val loadedCount: Int ): AllScreenIntent()
 
     data class LoadAllCharacterSeriesScreen(val characterId:Int, val loadedCount:Int):AllScreenIntent()
