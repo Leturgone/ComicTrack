9a59d51a0ad6f956c1e60e7867ae7e76831efd1d
Leturgone <finekon221@gmail.com>
2025-05-05 23:37:43 +0300

    Разделены интенты

--- app/src/main/java/com/example/comictracker/presentation/mvi/intents/AllScreenIntent.kt
+++ app/src/main/java/com/example/comictracker/presentation/mvi/intents/AllScreenIntent.kt
@@ -4,4 +4,24 @@ sealed class AllScreenIntent {
     data class LoadAllScreen(val sourceId: Int, val sourceCat:String, val loadedCount: Int ): AllScreenIntent()
 
     data class LoadAllCharactersScreen(val loadedCount: Int ): AllScreenIntent()
+
+    data class LoadAllCharacterSeriesScreen(val characterId:Int, val loadedCount:Int):AllScreenIntent()
+
+    data class LoadAllDiscoverSeriesScreen(val loadedCount: Int):AllScreenIntent()
+
+    data class LoadAllMayLikeSeriesScreen(val loadedCount: Int):AllScreenIntent()
+
+    data class LoadAllNextComicScreen(val loadedCount: Int):AllScreenIntent()
+
+    data class LoadAllNewComicScreen(val loadedCount: Int):AllScreenIntent()
+
+    data class LoadAllLastComicScreen(val loadedCount: Int):AllScreenIntent()
+
+    data class LoadAllCurrentReadingSeriesScreen(val loadedCount: Int):AllScreenIntent()
+
+    data class LoadAllLibComicScreen(val loadedCount: Int):AllScreenIntent()
+
+    data class LoadAllLibSeriesScreen(val loadedCount: Int):AllScreenIntent()
+
+    data class LoadReadListScreen(val loadedCount: Int):AllScreenIntent()
 }
