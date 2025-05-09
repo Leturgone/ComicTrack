f82df8867a7cb6aa01903ba5a4278b807afde040
Leturgone <finekon221@gmail.com>
2025-05-09 13:58:41 +0300

    added tests for loadCurrentReadIds

--- app/src/test/java/com/example/comictracker/localRepTests/LocalReadRepositoryTest.kt
+++ app/src/test/java/com/example/comictracker/localRepTests/LocalReadRepositoryTest.kt
@@ -3,23 +3,49 @@ package com.example.comictracker.localRepTests
 import com.example.comictracker.data.database.dao.ComicsDao
 import com.example.comictracker.data.database.dao.SeriesDao
 import com.example.comictracker.data.database.dao.SeriesListDao
+import com.example.comictracker.data.repository.local.LocalReadRepositoryImpl
+import com.example.comictracker.domain.repository.local.LocalReadRepository
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mockito
 
 class LocalReadRepositoryTest {
     private lateinit var comicsDao: ComicsDao
     private lateinit var seriesDao: SeriesDao
     private lateinit var seriesListDao: SeriesListDao
+    private lateinit var localReadRepository:LocalReadRepository
 
     @Before
-    fun setUp(){}
+    fun setUp(){
+        comicsDao = Mockito.mock(ComicsDao::class.java)
+        seriesDao = Mockito.mock(SeriesDao::class.java)
+        seriesListDao = Mockito.mock(SeriesListDao::class.java)
+        localReadRepository = LocalReadRepositoryImpl(comicsDao,seriesDao,seriesListDao)
+    }
 
 
     @Test
-    fun loadCurrentReadIdsSuccessTest(){}
+    fun loadCurrentReadIdsSuccessTest() = runTest{
+        Mockito.`when`(
+            seriesListDao.getCurrentlyReadingSeriesApiIds(0)
+        ).thenReturn(listOf(1,2,3))
+
+        val result = localReadRepository.loadCurrentReadIds(0)
+        assertEquals(listOf(1,2,3),result)
+    }
 
     @Test
-    fun loadCurrentReadIdsErrorTest(){}
+    fun loadCurrentReadIdsErrorTest() = runTest{
+        Mockito.`when`(
+            seriesListDao.getCurrentlyReadingSeriesApiIds(0)
+        ).thenReturn(emptyList())
+
+        val result = localReadRepository.loadCurrentReadIds(0)
+        assertEquals(emptyList<Int>(),result)
+    }
 
     @Test
     fun loadNextReadComicIdsSuccessTest(){}
