c0f60780637f1685d3453490a2b248bda75c79f1
Leturgone <finekon221@gmail.com>
2025-05-09 12:48:38 +0300

    added future tests for local read repository

---
+++ app/src/test/java/com/example/comictracker/localRepTests/LocalReadRepositoryTest.kt
@@ -0,0 +1,92 @@
+package com.example.comictracker.localRepTests
+
+import com.example.comictracker.data.database.dao.ComicsDao
+import com.example.comictracker.data.database.dao.SeriesDao
+import com.example.comictracker.data.database.dao.SeriesListDao
+import org.junit.Before
+import org.junit.Test
+
+class LocalReadRepositoryTest {
+    private lateinit var comicsDao: ComicsDao
+    private lateinit var seriesDao: SeriesDao
+    private lateinit var seriesListDao: SeriesListDao
+
+    @Before
+    fun setUp(){}
+
+
+    @Test
+    fun loadCurrentReadIdsSuccessTest(){}
+
+    @Test
+    fun loadCurrentReadIdsErrorTest(){}
+
+    @Test
+    fun loadNextReadComicIdsSuccessTest(){}
+
+    @Test
+    fun loadNextReadComicIdsErrorTest(){}
+
+    @Test
+    fun loadHistorySuccessTest(){}
+
+    @Test
+    fun loadHistoryErrorTest(){}
+
+    @Test
+    fun loadAllReadComicIdsSuccessTest(){}
+
+    @Test
+    fun loadAllReadComicIdsErrorTest(){}
+
+    @Test
+    fun loadAllReadSeriesIdsSuccessTest(){}
+
+    @Test
+    fun loadAllReadSeriesIdsErrorTest(){}
+
+    @Test
+    fun loadWillBeReadIdsSuccessTest(){}
+
+    @Test
+    fun loadWillBeReadIdsErrorTest(){}
+
+    @Test
+    fun loadStatisticsSuccessTest(){}
+
+    @Test
+    fun loadStatisticsErrorTest(){}
+
+    @Test
+    fun loadFavoritesIdsSuccessTest(){}
+
+    @Test
+    fun loadFavoritesIdsErrorTest(){}
+
+    @Test
+    fun loadComicMarkSuccessTest(){}
+
+    @Test
+    fun loadComicMarkErrorTest(){}
+
+    @Test
+    fun loadSeriesMarkSuccessTest(){}
+
+    @Test
+    fun loadSeriesMarkErrorTest(){}
+
+    @Test
+    fun loadSeriesFavoriteMarkSuccessTest(){}
+
+    @Test
+    fun loadSeriesFavoriteMarkErrorTest(){}
+
+    @Test
+    fun loadNextReadSuccessTest(){}
+
+    @Test
+    fun loadNextReadErrorTest(){}
+
+
+
+}
