7f90c5b4b263e4809a14fc0c3ec90f8819986c0d
Leturgone <finekon221@gmail.com>
2025-06-13 19:14:07 +0300

    added results to repository

--- app/src/main/java/com/example/comictracker/domain/repository/remote/RemoteComicsRepository.kt
+++ app/src/main/java/com/example/comictracker/domain/repository/remote/RemoteComicsRepository.kt
@@ -4,17 +4,17 @@ import com.example.comictracker.domain.model.ComicModel
 
 interface RemoteComicsRepository {
 
-    suspend fun getSeriesLastReleasesById(id: Int): List<ComicModel>
+    suspend fun getSeriesLastReleasesById(id: Int): Result<List<ComicModel>>
 
-    suspend fun getComicsFromSeries(seriesId: Int,loadedCount: Int = 0): List<ComicModel>
+    suspend fun getComicsFromSeries(seriesId: Int,loadedCount: Int = 0): Result<List<ComicModel>>
 
-    suspend fun getComicById(comicId:Int): ComicModel
+    suspend fun getComicById(comicId:Int): Result<ComicModel>
 
-    suspend fun fetchComics(ids: List<Int>): List<ComicModel>
+    suspend fun fetchComics(ids: List<Int>): Result<List<ComicModel>>
 
-    suspend fun fetchUpdatesForSeries(ids: List<Int>): List<ComicModel>
+    suspend fun fetchUpdatesForSeries(ids: List<Int>): Result<List<ComicModel>>
 
-    suspend fun getPreviousComicId(seriesId: Int, number: Int): Int?
+    suspend fun getPreviousComicId(seriesId: Int, number: Int): Result<Int?>
 
-    suspend fun getNextComicId(seriesId: Int, number: Int): Int?
+    suspend fun getNextComicId(seriesId: Int, number: Int): Result<Int?>
 }
