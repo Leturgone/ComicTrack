afa9c5cefb992c4dbe673ff52d6acdbae91d8346
Leturgone <finekon221@gmail.com>
2025-06-15 15:28:48 +0300

    added exceptions

---
+++ app/src/main/java/com/example/comictracker/data/repository/local/LocalWriteExceptions.kt
@@ -0,0 +1,9 @@
+package com.example.comictracker.data.repository.local
+
+class AlreadyMarkedException: Exception()
+
+class SeriesNotFoundException: Exception()
+
+class ComicNotFoundException: Exception()
+
+class EntityNotFoundException: Exception()
