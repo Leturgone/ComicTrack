ea83bf7dacc0a0ebdcffed04eb77c33e340e3b86
Leturgone <finekon221@gmail.com>
2025-06-15 16:40:09 +0300

    added result to repository

--- app/src/main/java/com/example/comictracker/domain/repository/local/LocalWriteRepository.kt
+++ app/src/main/java/com/example/comictracker/domain/repository/local/LocalWriteRepository.kt
@@ -1,19 +1,19 @@
 package com.example.comictracker.domain.repository.local
 
 interface LocalWriteRepository {
-    suspend fun markSeriesRead(apiId: Int):Boolean
+    suspend fun markSeriesRead(apiId: Int): Result<Unit>
 
-    suspend fun markComicRead(apiId: Int,seriesApiId:Int,nextComicApiId:Int?):Boolean
+    suspend fun markComicRead(apiId: Int,seriesApiId:Int,nextComicApiId:Int?):Result<Unit>
 
-    suspend fun addSeriesToFavorite(apiId: Int):Boolean
+    suspend fun addSeriesToFavorite(apiId: Int):Result<Unit>
 
-    suspend fun removeSeriesFromFavorite(apiId: Int):Boolean
+    suspend fun removeSeriesFromFavorite(apiId: Int):Result<Unit>
 
-    suspend fun addSeriesToCurrentlyRead(apiId: Int,firstIssueId:Int?):Boolean
+    suspend fun addSeriesToCurrentlyRead(apiId: Int,firstIssueId:Int?):Result<Unit>
 
-    suspend fun addSeriesToWillBeRead(apiId: Int):Boolean
+    suspend fun addSeriesToWillBeRead(apiId: Int):Result<Unit>
 
-    suspend fun markComicUnread(apiId: Int,seriesApiId:Int,prevComicApiId:Int?):Boolean
+    suspend fun markComicUnread(apiId: Int,seriesApiId:Int,prevComicApiId:Int?):Result<Unit>
 
-    suspend fun markSeriesUnread(apiId: Int):Boolean
+    suspend fun markSeriesUnread(apiId: Int):Result<Unit>
 }
