0198fd29825fe89637a31740be2bcbe7667f53e6
Leturgone <finekon221@gmail.com>
2025-06-16 01:11:53 +0300

    added result to repository

--- app/src/main/java/com/example/comictracker/domain/repository/local/LocalReadRepository.kt
+++ app/src/main/java/com/example/comictracker/domain/repository/local/LocalReadRepository.kt
@@ -3,29 +3,29 @@ package com.example.comictracker.domain.repository.local
 import com.example.comictracker.domain.model.StatisticsforAll
 
 interface LocalReadRepository {
-    suspend fun loadCurrentReadIds(offset:Int):List<Int>
 
-    suspend fun loadNextReadComicIds(offset:Int):List<Int>
+    suspend fun loadCurrentReadIds(offset:Int): Result<List<Int>>
 
+    suspend fun loadNextReadComicIds(offset:Int): Result<List<Int>>
 
-    suspend fun loadHistory(offset:Int):List<Int>
+    suspend fun loadHistory(offset:Int): Result<List<Int>>
 
-    suspend fun loadAllReadComicIds(offset:Int):List<Int>
+    suspend fun loadAllReadComicIds(offset:Int): Result<List<Int>>
 
-    suspend fun loadAllReadSeriesIds(offset:Int):List<Int>
+    suspend fun loadAllReadSeriesIds(offset:Int): Result<List<Int>>
 
-    suspend fun loadWillBeReadIds(offset:Int):List<Int>
+    suspend fun loadWillBeReadIds(offset:Int): Result<List<Int>>
 
-    suspend fun loadStatistics(): StatisticsforAll
+    suspend fun loadStatistics(): Result<StatisticsforAll>
 
-    suspend fun loadFavoritesIds():List<Int>
+    suspend fun loadFavoritesIds(): Result<List<Int>>
 
-    suspend fun loadComicMark(apiId: Int):String
+    suspend fun loadComicMark(apiId: Int): Result<String>
 
-    suspend fun loadSeriesMark(apiId:Int):String
+    suspend fun loadSeriesMark(apiId:Int): Result<String>
 
-    suspend fun loadSeriesFavoriteMark(apiId: Int):Boolean
+    suspend fun loadSeriesFavoriteMark(apiId: Int): Result<Boolean>
 
-    suspend fun loadNextRead(seriesApiId: Int):Int?
+    suspend fun loadNextRead(seriesApiId: Int): Result<Int?>
 
 }
