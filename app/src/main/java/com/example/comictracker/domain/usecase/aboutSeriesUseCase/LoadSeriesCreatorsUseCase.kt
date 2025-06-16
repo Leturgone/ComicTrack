8b2924f24955e5e82154935d0260360c5747cd02
Leturgone <finekon221@gmail.com>
2025-06-17 00:36:47 +0300

    added LoadSeriesCreatorsUseCase

---
+++ app/src/main/java/com/example/comictracker/domain/usecase/aboutSeriesUseCase/LoadSeriesCreatorsUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.comictracker.domain.usecase.aboutSeriesUseCase
+
+import com.example.comictracker.domain.model.SeriesModel
+import com.example.comictracker.domain.repository.remote.RemoteCreatorsRepository
+
+class LoadSeriesCreatorsUseCase(
+    private val remoteCreatorsRepository: RemoteCreatorsRepository
+) {
+    suspend operator fun invoke(series: SeriesModel) = remoteCreatorsRepository.getSeriesCreators(series.creators?: emptyList())
+}
