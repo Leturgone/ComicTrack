39a8334507fd2afbc25c5f7d0613d4cffa7bead4
Leturgone <finekon221@gmail.com>
2025-06-16 16:44:00 +0300

    added LoadCharacterDataUseCase

--- app/src/main/java/com/example/comictracker/domain/usecase/characterUseCases/LoadCharacterDataUseCase.kt
+++ app/src/main/java/com/example/comictracker/domain/usecase/characterUseCases/LoadCharacterDataUseCase.kt
@@ -1,16 +1,14 @@
 package com.example.comictracker.domain.usecase.characterUseCases
 
+import com.example.comictracker.domain.model.CharacterModel
 import com.example.comictracker.domain.repository.remote.RemoteCharacterRepository
 import com.example.comictracker.domain.repository.remote.RemoteSeriesRepository
 import kotlinx.coroutines.async
 
 class LoadCharacterDataUseCase(
-    private val characterRepository: RemoteCharacterRepository,
-    private val seriesRepository: RemoteSeriesRepository
+    private val characterRepository: RemoteCharacterRepository
 ) {
-    suspend operator fun invoke(characterId:Int){
-        return async{
-            characterRepository.getCharacterById(characterId)
-        }
+    suspend operator fun invoke(characterId:Int): Result<CharacterModel> {
+        return characterRepository.getCharacterById(characterId)
     }
 }
