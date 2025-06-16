e3bfe1319aa37c7bb740b31e925dea5eb8defd4c
Leturgone <finekon221@gmail.com>
2025-06-17 00:34:54 +0300

    refactored with model

--- app/src/main/java/com/example/comictracker/domain/usecase/aboutComicUseCases/LoadComicCreatorsUseCase.kt
+++ app/src/main/java/com/example/comictracker/domain/usecase/aboutComicUseCases/LoadComicCreatorsUseCase.kt
@@ -1,9 +1,10 @@
 package com.example.comictracker.domain.usecase.aboutComicUseCases
 
+import com.example.comictracker.domain.model.ComicModel
 import com.example.comictracker.domain.repository.remote.RemoteCreatorsRepository
 
 class LoadComicCreatorsUseCase(
     private val remoteCreatorsRepository: RemoteCreatorsRepository
 ) {
-    suspend operator fun invoke(creators: List<Pair<Int, String>>) = remoteCreatorsRepository.getComicCreators(creators)
+    suspend operator fun invoke(comic:ComicModel) = remoteCreatorsRepository.getComicCreators(comic.creators)
 }
