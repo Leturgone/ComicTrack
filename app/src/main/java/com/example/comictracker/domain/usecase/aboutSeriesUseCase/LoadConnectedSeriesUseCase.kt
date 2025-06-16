65bd4e17cae11aceb77a3beadee61dcbfca05ef2
Leturgone <finekon221@gmail.com>
2025-06-17 00:36:08 +0300

    added LoadConnectedSeriesUseCase

---
+++ app/src/main/java/com/example/comictracker/domain/usecase/aboutSeriesUseCase/LoadConnectedSeriesUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.comictracker.domain.usecase.aboutSeriesUseCase
+
+import com.example.comictracker.domain.model.SeriesModel
+import com.example.comictracker.domain.repository.remote.RemoteSeriesRepository
+
+class LoadConnectedSeriesUseCase(
+    private val remoteSeriesRepository: RemoteSeriesRepository
+) {
+    suspend operator fun invoke(series:SeriesModel) = remoteSeriesRepository.getConnectedSeries(series.connectedSeries)
+}
