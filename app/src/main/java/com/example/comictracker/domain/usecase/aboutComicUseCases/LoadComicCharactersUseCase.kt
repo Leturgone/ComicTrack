12aba74893e97b04a70805234dfebdbc05630d2b
Leturgone <finekon221@gmail.com>
2025-06-16 20:52:07 +0300

    added LoadComicCharactersUseCase

---
+++ app/src/main/java/com/example/comictracker/domain/usecase/aboutComicUseCases/LoadComicCharactersUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.comictracker.domain.usecase.aboutComicUseCases
+
+import com.example.comictracker.domain.model.CharacterModel
+import com.example.comictracker.domain.repository.remote.RemoteCharacterRepository
+
+class LoadComicCharactersUseCase(
+    private val remoteCharacterRepository: RemoteCharacterRepository
+) {
+    suspend operator fun invoke(comicId:Int): Result<List<CharacterModel>> {
+        return remoteCharacterRepository.getComicCharacters(comicId)
+    }
+}
