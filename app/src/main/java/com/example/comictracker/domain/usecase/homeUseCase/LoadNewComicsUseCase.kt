baceb1eed6512e20589e2013dec033124f8582e9
Leturgone <finekon221@gmail.com>
2025-06-20 22:52:55 +0300

    added LoadNewComicsUseCase

---
+++ app/src/main/java/com/example/comictracker/domain/usecase/homeUseCase/LoadNewComicsUseCase.kt
@@ -0,0 +1,20 @@
+package com.example.comictracker.domain.usecase.homeUseCase
+
+import com.example.comictracker.domain.model.ComicModel
+import com.example.comictracker.domain.repository.local.LocalReadRepository
+import com.example.comictracker.domain.repository.remote.RemoteComicsRepository
+
+class LoadNewComicsUseCase(
+    private val remoteComicsRepository: RemoteComicsRepository,
+    private val localReadRepository: LocalReadRepository
+) {
+    suspend operator fun invoke(): Result<List<ComicModel>>{
+        val loadedIdsSeriesFromBDDef = localReadRepository.loadCurrentReadIds(0)
+        return loadedIdsSeriesFromBDDef.fold(
+            onSuccess = {
+                remoteComicsRepository.fetchUpdatesForSeries(it)
+            },
+            onFailure = {Result.failure(it)}
+        )
+    }
+}
