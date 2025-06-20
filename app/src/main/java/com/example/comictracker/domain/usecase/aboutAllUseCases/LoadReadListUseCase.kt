4f2827a5e8e7c49e082b152d6a0f8f6c394b5996
Leturgone <finekon221@gmail.com>
2025-06-20 16:50:40 +0300

    added LoadReadListUseCase

--- app/src/main/java/com/example/comictracker/domain/usecase/aboutAllUseCases/LoadReadListUseCase.kt
+++ app/src/main/java/com/example/comictracker/domain/usecase/aboutAllUseCases/LoadReadListUseCase.kt
@@ -1,4 +1,19 @@
 package com.example.comictracker.domain.usecase.aboutAllUseCases
 
-class LoadReadListUseCase {
+import com.example.comictracker.domain.repository.local.LocalReadRepository
+import com.example.comictracker.domain.repository.remote.RemoteSeriesRepository
+import com.example.comictracker.presentation.mvi.DataState
+
+class LoadReadListUseCase(
+    private val remoteSeriesRepository: RemoteSeriesRepository,
+    private val localReadRepository: LocalReadRepository
+) {
+    suspend operator fun invoke(loadedCount:Int){
+        return localReadRepository.loadWillBeReadIds(loadedCount).fold(
+            onSuccess = {readlistFromBD->
+                remoteSeriesRepository.fetchSeries(readlistFromBD)
+            },
+            onFailure = { DataState.Error("Error while loading all readlist")}
+        )
+    }
 }
