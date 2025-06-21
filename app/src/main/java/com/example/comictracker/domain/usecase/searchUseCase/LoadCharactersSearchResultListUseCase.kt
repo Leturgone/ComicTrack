07b350ad8033a24dfe42325271525ea0a6687f36
Leturgone <finekon221@gmail.com>
2025-06-21 22:29:24 +0300

    added LoadCharactersSearchResultListUseCase

---
+++ app/src/main/java/com/example/comictracker/domain/usecase/searchUseCase/LoadCharactersSearchResultListUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.comictracker.domain.usecase.searchUseCase
+
+import com.example.comictracker.domain.repository.remote.RemoteCharacterRepository
+
+class LoadCharactersSearchResultListUseCase(
+    private val remoteCharacterRepository: RemoteCharacterRepository
+) {
+    suspend operator fun invoke(query: String) = remoteCharacterRepository.getCharactersByName(query)
+}
