67dbd01bc27c3ab86a1177f0e1b4a7847d07992d
Leturgone <finekon221@gmail.com>
2025-06-21 21:54:44 +0300

    added LoadHistoryReadComicUseCase

---
+++ app/src/main/java/com/example/comictracker/domain/usecase/libraryUseCase/LoadHistoryReadComicUseCase.kt
@@ -0,0 +1,20 @@
+package com.example.comictracker.domain.usecase.libraryUseCase
+
+import com.example.comictracker.domain.model.ComicModel
+import com.example.comictracker.domain.repository.local.LocalReadRepository
+import com.example.comictracker.domain.repository.remote.RemoteComicsRepository
+
+class LoadHistoryReadComicUseCase(
+    private val remoteComicsRepository: RemoteComicsRepository
+    private val localReadRepository: LocalReadRepository
+) {
+    suspend operator fun invoke(): Result<List<ComicModel>>{
+        val loadedHistoryReadComicFromBD = localReadRepository.loadHistory(0)
+        return loadedHistoryReadComicFromBD.fold(
+            onSuccess = {idList ->
+                remoteComicsRepository.fetchComics(idList)
+            },
+            onFailure = {Result.failure(it)}
+        )
+    }
+}
