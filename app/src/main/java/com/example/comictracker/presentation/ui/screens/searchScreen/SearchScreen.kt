69546ab69d67f24cec9d6cb72c23a313b161fc27
Leturgone <finekon221@gmail.com>
2025-06-25 23:50:28 +0300

    refactored states in search screen

--- app/src/main/java/com/example/comictracker/presentation/ui/screens/searchScreen/SearchScreen.kt
+++ app/src/main/java/com/example/comictracker/presentation/ui/screens/searchScreen/SearchScreen.kt
@@ -1,6 +1,9 @@
 package com.example.comictracker.presentation.ui.screens.searchScreen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
@@ -12,6 +15,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.comictracker.presentation.mvi.ComicAppState
@@ -27,48 +31,68 @@ fun SearchScreen(navController: NavHostController,
 
     val uiState by viewModel.state.collectAsState()
     var showMayLikeToast by remember { mutableStateOf(false) }
+    var mayLikeErrorMessage by remember { mutableStateOf("") }
     var showDiscoverToast by remember { mutableStateOf(false) }
+    var discoverErrorMessage by remember { mutableStateOf("") }
     var showCharacterToast by remember { mutableStateOf(false) }
+    var characterErrorMessage by remember { mutableStateOf("") }
 
     LaunchedEffect(key1 = "searchscreen") {
         viewModel.processIntent(SearchScreenIntent.LoadSearchScreen)
     }
-
-    Column {
-        SearchSec(navController)
-        Column(Modifier.verticalScroll(rememberScrollState())) {
-            uiState.let {state -> 
-                when(state){
-                    is ComicAppState.SearchScreenState ->{
-                        when(state.mayLikeList){
-                            is DataState.Error -> CustomToastMessage(
-                                message = state.mayLikeList.errorMessage,
-                                isVisible = showMayLikeToast,
-                                onDismiss = { showMayLikeToast = false })
-                            DataState.Loading -> CircularProgressIndicator()
-                            is DataState.Success -> MayLikeSec(state.mayLikeList.result,navController)
+    Box(){
+        Column {
+            SearchSec(navController)
+            Column(Modifier.verticalScroll(rememberScrollState())) {
+                uiState.let {state ->
+                    when(state){
+                        is ComicAppState.SearchScreenState ->{
+                            when(state.mayLikeList){
+                                is DataState.Error -> LaunchedEffect(Unit){
+                                    mayLikeErrorMessage = state.mayLikeList.errorMessage
+                                    showMayLikeToast = true
+                                }
+                                DataState.Loading -> CircularProgressIndicator()
+                                is DataState.Success -> MayLikeSec(state.mayLikeList.result,navController)
+                            }
+                            when(state.discoverList){
+                                is DataState.Error -> LaunchedEffect(Unit){
+                                    discoverErrorMessage = state.discoverList.errorMessage
+                                    showDiscoverToast = true
+                                }
+                                DataState.Loading -> CircularProgressIndicator()
+                                is DataState.Success -> DiscoverSec(state.discoverList.result,navController)
+                            }
+                            when(state.charactersSec){
+                                is DataState.Error -> LaunchedEffect(Unit){
+                                    characterErrorMessage = state.charactersSec.errorMessage
+                                    showCharacterToast = true
+                                }
+                                DataState.Loading -> CircularProgressIndicator()
+                                is DataState.Success -> CharacterSec(state.charactersSec.result,navController)
+                            }
                         }
-                        when(state.discoverList){
-                            is DataState.Error -> CustomToastMessage(
-                                message = state.discoverList.errorMessage,
-                                isVisible = showDiscoverToast,
-                                onDismiss = { showDiscoverToast = false })
-                            DataState.Loading -> CircularProgressIndicator()
-                            is DataState.Success -> DiscoverSec(state.discoverList.result,navController)
-                        }
-                        when(state.charactersSec){
-                            is DataState.Error -> CustomToastMessage(
-                                message = state.charactersSec.errorMessage,
-                                isVisible = showCharacterToast,
-                                onDismiss = { showCharacterToast = false })
-                            DataState.Loading -> CircularProgressIndicator()
-                            is DataState.Success -> CharacterSec(state.charactersSec.result,navController)
-                        }
+                        else -> {
+                            CircularProgressIndicator()}
                     }
-                    else -> {
-                        CircularProgressIndicator()}
                 }
             }
+        }
+        Column {
+            CustomToastMessage(
+                message = mayLikeErrorMessage,
+                isVisible = showMayLikeToast,
+                onDismiss = { showMayLikeToast = false })
+            Spacer(modifier = Modifier.height(12.dp))
+            CustomToastMessage(
+                message = discoverErrorMessage,
+                isVisible = showDiscoverToast,
+                onDismiss = { showDiscoverToast = false })
+            Spacer(modifier = Modifier.height(12.dp))
+            CustomToastMessage(
+                message = characterErrorMessage,
+                isVisible = showCharacterToast,
+                onDismiss = { showCharacterToast = false })
         }
     }
 }
