8ec69575ca3677678876fb61d0db2a9748bc3a1d
Leturgone <finekon221@gmail.com>
2025-06-27 21:40:58 +0300

    added checking in use case

--- app/src/main/java/com/example/comictracker/domain/usecase/aboutSeriesUseCases/AddSeriesToFavoritesUseCase.kt
+++ app/src/main/java/com/example/comictracker/domain/usecase/aboutSeriesUseCases/AddSeriesToFavoritesUseCase.kt
@@ -1,10 +1,24 @@
 package com.example.comictracker.domain.usecase.aboutSeriesUseCases
 
+import com.example.comictracker.domain.repository.local.LocalReadRepository
 import com.example.comictracker.domain.repository.local.LocalWriteRepository
 
 class AddSeriesToFavoritesUseCase(
-    private val localWriteRepository: LocalWriteRepository
+    private val localWriteRepository: LocalWriteRepository,
+    private val localReadRepository: LocalReadRepository
 ) {
 
-    suspend operator fun invoke(apiId: Int) =  localWriteRepository.addSeriesToFavorite(apiId)
+    suspend operator fun invoke(apiId: Int): Result<Unit>{
+        return localReadRepository.loadFavoritesCount().fold(
+            onSuccess = { favoritesCount ->
+                if (favoritesCount<4){
+                    localWriteRepository.addSeriesToFavorite(apiId)
+                }else{
+                    Result.failure(Exception())
+                }},
+            onFailure = {
+                Result.failure(it)
+            }
+        )
+    }
 }
